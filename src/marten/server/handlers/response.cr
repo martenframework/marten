be795cca6894ce694ab435b51e8774efe4ec7ffd
Morgan Aubert <morgan@aubert.email>
2020-03-07 22:36:33 -0500

    Fix indentation

--- src/marten/server/handlers/response.cr
+++ src/marten/server/handlers/response.cr
@@ -1,25 +1,25 @@
 module Marten
-    module Server
-      module Handlers
-        class Response
-          include ::HTTP::Handler
+  module Server
+    module Handlers
+      class Response
+        include ::HTTP::Handler
 
-          def call(context : ::HTTP::Server::Context)
-            process(context)
-          end
+        def call(context : ::HTTP::Server::Context)
+          process(context)
+        end
 
-          private def process(context)
-            response = context.marten.response
-            return context if response.nil?
+        private def process(context)
+          response = context.marten.response
+          return context if response.nil?
 
-            context.response.status_code = response.status
-            context.response.headers.merge!(response.headers)
-            context.response.content_type = response.content_type.to_s
-            context.response.print(response.content)
+          context.response.status_code = response.status
+          context.response.headers.merge!(response.headers)
+          context.response.content_type = response.content_type.to_s
+          context.response.print(response.content)
 
-            context
-          end
+          context
         end
       end
     end
   end
+end

a50ecd59963486064e69bdc32dbfe63d3a195190
Morgan Aubert <morgan@aubert.email>
2020-03-12 16:06:25 -0400

    Add test routes with parameters

--- example_project/config/routes.cr
+++ example_project/config/routes.cr
@@ -1,5 +1,7 @@
 Marten.routes.draw do
   path "/", Blog::Views::Home, name: "home"
   path "/home/bis", Blog::Views::Home, name: "home_bis"
+  path "/home/<id:str>/test", Blog::Views::Home, name: "home_with_arg"
   path "/included", Blog::ROUTES, name: "included"
+  path "/included/<id:str>/test", Blog::ROUTES, name: "included_with_arg"
 end

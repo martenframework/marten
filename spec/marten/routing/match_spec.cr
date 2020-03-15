4481dfd9114e1d6ad407dceae1a45bb21bec3e51
Morgan Aubert <morgan@aubert.email>
2020-03-14 22:45:49 -0400

    Wrap necessary specs in modules

--- spec/marten/routing/match_spec.cr
+++ spec/marten/routing/match_spec.cr
@@ -1,32 +1,28 @@
 require "./spec_helper"
 
-describe Marten::Routing::Match do
-  describe "::view" do
-    it "returns the associated view class" do
-      match = Marten::Routing::Match.new(
-        Marten::Routing::MatchSpec::TestView,
-        { "id" => 123 } of String => Marten::Routing::Parameter::Types
-      )
-      match.view.should eq Marten::Routing::MatchSpec::TestView
-    end
-  end
-
-  describe "::kwargs" do
-    it "returns the associated view parameters" do
-      match = Marten::Routing::Match.new(
-        Marten::Routing::MatchSpec::TestView,
-        { "id" => 123 } of String => Marten::Routing::Parameter::Types
-      )
-      match.kwargs.should eq({ "id" => 123 })
-    end
-  end
-end
-
-module Marten
-  module Routing
-    module MatchSpec
-      class TestView < Marten::Views::Base
+module Marten::Routing::MatchSpec
+  describe Marten::Routing::Match do
+    describe "::view" do
+      it "returns the associated view class" do
+        match = Marten::Routing::Match.new(
+          TestView,
+          { "id" => 123 } of String => Marten::Routing::Parameter::Types
+        )
+        match.view.should eq TestView
       end
     end
+
+    describe "::kwargs" do
+      it "returns the associated view parameters" do
+        match = Marten::Routing::Match.new(
+          TestView,
+          { "id" => 123 } of String => Marten::Routing::Parameter::Types
+        )
+        match.kwargs.should eq({ "id" => 123 })
+      end
+    end
+  end
+
+  class TestView < Marten::Views::Base
   end
 end
--- spec/marten/routing/parameter_spec.cr
+++ spec/marten/routing/parameter_spec.cr
@@ -1,50 +1,42 @@
 require "./spec_helper"
 
-describe Marten::Routing::Parameter do
-  describe "::register" do
-    it "allows to register a new path parameter implementation" do
-      Marten::Routing::Parameter.register("foo", Marten::Routing::ParameterSpec::FooParameter)
-      Marten::Routing::Parameter.registry["foo"].should(
-        be_a(Marten::Routing::ParameterSpec::FooParameter)
-      )
-      Marten::Routing::Parameter.registry.delete("foo")
-    end
-
-    it "allows to register a new path parameter implementation using a symbol identifier" do
-      Marten::Routing::Parameter.register(:foo, Marten::Routing::ParameterSpec::FooParameter)
-      Marten::Routing::Parameter.registry["foo"].should(
-        be_a(Marten::Routing::ParameterSpec::FooParameter)
-      )
-      Marten::Routing::Parameter.registry.delete("foo")
-    end
-  end
-
-  describe "::registry" do
-    it "returns the registered path parameter implementations" do
-      Marten::Routing::Parameter.registry.size.should eq 3
-      Marten::Routing::Parameter.registry["str"].should be_a Marten::Routing::Parameter::String
-      Marten::Routing::Parameter.registry["int"].should be_a Marten::Routing::Parameter::Integer
-      Marten::Routing::Parameter.registry["slug"].should be_a Marten::Routing::Parameter::Slug
-    end
-  end
-end
-
-module Marten
-  module Routing
-    module ParameterSpec
-      class FooParameter < Marten::Routing::Parameter::Base
-        def regex : Regex
-          /.+/
-        end
-
-        def loads(value : String) : String
-          value
-        end
-
-        def dumps(value : String) : String
-          value
-        end
+module Marten::Routing::ParameterSpec
+  describe Marten::Routing::Parameter do
+    describe "::register" do
+      it "allows to register a new path parameter implementation" do
+        Marten::Routing::Parameter.register("foo", FooParameter)
+        Marten::Routing::Parameter.registry["foo"].should be_a FooParameter
+        Marten::Routing::Parameter.registry.delete("foo")
       end
+
+      it "allows to register a new path parameter implementation using a symbol identifier" do
+        Marten::Routing::Parameter.register(:foo, FooParameter)
+        Marten::Routing::Parameter.registry["foo"].should be_a FooParameter
+        Marten::Routing::Parameter.registry.delete("foo")
+      end
+    end
+
+    describe "::registry" do
+      it "returns the registered path parameter implementations" do
+        Marten::Routing::Parameter.registry.size.should eq 3
+        Marten::Routing::Parameter.registry["str"].should be_a Marten::Routing::Parameter::String
+        Marten::Routing::Parameter.registry["int"].should be_a Marten::Routing::Parameter::Integer
+        Marten::Routing::Parameter.registry["slug"].should be_a Marten::Routing::Parameter::Slug
+      end
+    end
+  end
+
+  class FooParameter < Marten::Routing::Parameter::Base
+    def regex : Regex
+      /.+/
+    end
+
+    def loads(value : String) : String
+      value
+    end
+
+    def dumps(value : String) : String
+      value
     end
   end
 end

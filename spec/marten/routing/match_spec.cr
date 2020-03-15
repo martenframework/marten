e2808cb0706ead4ea9a80141354d8255dd3b4dd2
Morgan Aubert <morgan@aubert.email>
2020-03-14 22:02:05 -0400

    Add unit tests for Routing::Match abstraction

---
+++ spec/marten/routing/match_spec.cr
@@ -0,0 +1,32 @@
+require "./spec_helper"
+
+describe Marten::Routing::Match do
+  describe "::view" do
+    it "returns the associated view class" do
+      match = Marten::Routing::Match.new(
+        Marten::Routing::MatchSpec::TestView,
+        { "id" => 123 } of String => Marten::Routing::Parameter::Types
+      )
+      match.view.should eq Marten::Routing::MatchSpec::TestView
+    end
+  end
+
+  describe "::kwargs" do
+    it "returns the associated view parameters" do
+      match = Marten::Routing::Match.new(
+        Marten::Routing::MatchSpec::TestView,
+        { "id" => 123 } of String => Marten::Routing::Parameter::Types
+      )
+      match.kwargs.should eq({ "id" => 123 })
+    end
+  end
+end
+
+module Marten
+  module Routing
+    module MatchSpec
+      class TestView < Marten::Views::Base
+      end
+    end
+  end
+end

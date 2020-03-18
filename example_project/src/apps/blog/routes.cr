8749ffaabd9a46916d0a0a5c468fbae025860b7d
Morgan Aubert <morgan@aubert.email>
2020-03-17 21:05:12 -0400

    Add extra route to the example project for test purpose

--- example_project/src/apps/blog/routes.cr
+++ example_project/src/apps/blog/routes.cr
@@ -1,5 +1,6 @@
 module Blog
   ROUTES = Marten::Routing::Map.draw do
     path "/test/xyx", Views::Home, name: "home"
+    path "/test/xyx/<number:int>", Views::Home, name: "home_with_int"
   end
 end

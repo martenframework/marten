bfeab8ea987c75c18972a1f98bf5b98f754c50f7
Morgan Aubert <morgan@aubert.email>
2020-03-22 21:59:08 -0400

    Ensure HTTP::Response subclasses can initialize all response parameters

--- spec/marten/http/response/not_allowed_spec.cr
+++ spec/marten/http/response/not_allowed_spec.cr
@@ -7,5 +7,17 @@ describe Marten::HTTP::Response::NotAllowed do
       response.status.should eq 405
       response.headers.should eq({ "Allow" => "GET, POST" })
     end
+
+    it "allows to initialize other response parameters" do
+      response = Marten::HTTP::Response::NotAllowed.new(
+        %w(GET POST),
+        content: "Test content",
+        content_type: "text/plain"
+      )
+      response.status.should eq 405
+      response.headers.should eq({ "Allow" => "GET, POST" })
+      response.content.should eq "Test content"
+      response.content_type.should eq "text/plain"
+    end
   end
 end
--- src/marten/http/response/not_allowed.cr
+++ src/marten/http/response/not_allowed.cr
@@ -2,8 +2,12 @@ module Marten
   module HTTP
     class Response
       class NotAllowed < Response
-        def initialize(allowed_methods : Array(String))
-          super(status: 405)
+        def initialize(
+            allowed_methods : Array(String),
+            content : String = "",
+            content_type : String = "text/html"
+        )
+          super(content: content, content_type: content_type, status: 405)
           self["Allow"] = allowed_methods.map(&.upcase).join(", ")
         end
       end

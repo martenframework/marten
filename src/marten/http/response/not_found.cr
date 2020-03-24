327882f2dcb3dd9d23fe207d7aec97a8d172c270
Morgan Aubert <morgan@aubert.email>
2020-03-23 21:33:32 -0400

    Add not found HTTP response abstraction

---
+++ src/marten/http/response/not_found.cr
@@ -0,0 +1,11 @@
+module Marten
+  module HTTP
+    class Response
+      class NotFound < Response
+        def initialize(content : String = "", content_type : String = DEFAULT_CONTENT_TYPE)
+          super(content: content, content_type: content_type, status: 404)
+        end
+      end
+    end
+  end
+end

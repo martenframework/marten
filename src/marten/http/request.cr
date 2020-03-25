47f0a303746bcbad48a6874a563dae4834de87bf
Morgan Aubert <morgan@aubert.email>
2020-03-24 21:42:31 -0400

    Add docstrings for HTTP::Request abstraction

--- src/marten/http/request.cr
+++ src/marten/http/request.cr
@@ -1,14 +1,24 @@
 module Marten
   module HTTP
+    # Represents an HTTP request processed by Marten.
+    #
+    # When a page is request, Marten creates a `Marten::HTTP::Request` that gives access to all the information and
+    # metadata of the incoming request.
     class Request
       def initialize(@request : ::HTTP::Request)
       end
 
-      def method
-        @request.method
+      # Returns a string representation of HTTP method that was used in the request.
+      #
+      # The returned method name (eg. "GET" or "POST") is completely uppercase.
+      def method : String
+        @request.method.upcase
       end
 
-      def path
+      # Returns the request path as a string.
+      #
+      # Only the path of the request is included (without scheme or domain).
+      def path : String
         @request.path
       end
     end

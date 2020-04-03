8d630dd972cbab027a00b25e9baf137188d478b5
Morgan Aubert <morgan@aubert.email>
2020-04-02 21:37:40 -0400

    Add HTTP redirect response abstractions

---
+++ src/marten/http/response/permanent_redirect.cr
@@ -0,0 +1,16 @@
+module Marten
+  module HTTP
+    class Response
+      class PermanentRedirect < Response
+        def initialize(
+          location : String,
+          content : String = "",
+          content_type : String = DEFAULT_CONTENT_TYPE
+        )
+          super(content: content, content_type: content_type, status: 301)
+          self["Location"] = location
+        end
+      end
+    end
+  end
+end
---
+++ src/marten/http/response/redirect.cr
@@ -0,0 +1,16 @@
+module Marten
+  module HTTP
+    class Response
+      class Redirect < Response
+        def initialize(
+          location : String,
+          content : String = "",
+          content_type : String = DEFAULT_CONTENT_TYPE
+        )
+          super(content: content, content_type: content_type, status: 302)
+          self["Location"] = location
+        end
+      end
+    end
+  end
+end

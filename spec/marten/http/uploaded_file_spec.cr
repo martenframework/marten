c59cdf26cb414981bd85c8f0ea76018c63d37209
Morgan Aubert <morgan@aubert.email>
2020-04-09 21:21:33 -0400

    Add basic implementation of uploaded files using tempfiles

---
+++ spec/marten/http/uploaded_file_spec.cr
@@ -0,0 +1,52 @@
+require "./spec_helper"
+
+describe Marten::HTTP::UploadedFile do
+  describe "::new" do
+    it "allows to initialize an uploaded file from a form data part" do
+      uploaded_file = Marten::HTTP::UploadedFile.new(
+        HTTP::FormData::Part.new(
+          HTTP::Headers{ "Content-Disposition" => %{form-data; name="file"; filename="a.txt"; size=123} },
+          IO::Memory.new
+        )
+      )
+      uploaded_file.size.should eq 123
+    end
+  end
+
+  describe "#filename" do
+    it "returns the filename" do
+      uploaded_file = Marten::HTTP::UploadedFile.new(
+        HTTP::FormData::Part.new(
+          HTTP::Headers{ "Content-Disposition" => %{form-data; name="file"; filename="a.txt"; size=123} },
+          IO::Memory.new
+        )
+      )
+      uploaded_file.filename.should eq "a.txt"
+    end
+  end
+
+  describe "#size" do
+    it "returns the file size specified in the content disposition" do
+      uploaded_file = Marten::HTTP::UploadedFile.new(
+        HTTP::FormData::Part.new(
+          HTTP::Headers{ "Content-Disposition" => %{form-data; name="file"; filename="a.txt"; size=123} },
+          IO::Memory.new
+        )
+      )
+      uploaded_file.size.should eq 123
+    end
+  end
+
+  describe "#io" do
+    it "returns the IO object allowing to manipulate the file content" do
+      uploaded_file = Marten::HTTP::UploadedFile.new(
+        HTTP::FormData::Part.new(
+          HTTP::Headers{ "Content-Disposition" => %{form-data; name="file"; filename="a.txt"; size=123} },
+          IO::Memory.new("This is a test")
+        )
+      )
+      uploaded_file.io.should be_a IO
+      uploaded_file.io.gets_to_end.should eq "This is a test"
+    end
+  end
+end
--- src/marten/http/uploaded_file.cr
+++ src/marten/http/uploaded_file.cr
@@ -1,8 +1,22 @@
 module Marten
   module HTTP
     class UploadedFile
+      @io : IO
+
+      getter io
+
       def initialize(@part : ::HTTP::FormData::Part)
+        @io = File.tempfile
+        ::File.open(@io.as(File).path, "w") do |file|
+          IO.copy(@part.body, file)
+        end
       end
+
+      # Returns the uploaded file name.
+      delegate filename, to: @part
+
+      # Returns the uploaded file size.
+      delegate size, to: @part
     end
   end
 end

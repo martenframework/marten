6531061987378debc7159befe6c7c34fc21c6909
Morgan Aubert <morgan@aubert.email>
2020-04-20 20:31:00 -0400

    Improve validation of existing database configuration options

--- src/marten/conf/global_settings/database.cr
+++ src/marten/conf/global_settings/database.cr
@@ -3,8 +3,8 @@ module Marten
     class GlobalSettings
       # Defines the configuration of a specific database connection.
       class Database
-        @backend : String | Symbol | Nil
-        @name : Path | String | Symbol | Nil
+        @backend : String?
+        @name : String?
 
         getter id
         getter backend
@@ -16,12 +16,26 @@ module Marten
         def initialize(@id : String)
         end
 
-        protected def validate
-          raise Errors::InvalidConfiguration.new("A database backend must be chosen") if backend.nil?
+        def backend=(val : String | Symbol)
+          @backend = val.to_s
+        end
 
-          unless DB::Connection::IMPLEMENTATIONS.has_key?(backend.not_nil!.to_s)
-            raise Errors::InvalidConfiguration.new("Unknown database backend: '#{backend}'")
+        def name=(val : Path | String | Symbol)
+          @name = val.to_s
+        end
+
+        protected def validate
+          raise_invalid_config("missing database backend") if backend.to_s.empty?
+
+          unless DB::Connection::IMPLEMENTATIONS.has_key?(backend)
+            raise_invalid_config("unknown database backend '#{backend}'")
           end
+
+          raise_invalid_config("missing database name") if name.to_s.empty?
+        end
+
+        private def raise_invalid_config(msg)
+          raise Errors::InvalidConfiguration.new("Invalid configuration for database '#{id}': #{msg}")
         end
       end
     end

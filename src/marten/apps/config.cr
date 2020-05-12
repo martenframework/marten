bb653ec87532d6d34419d0b0724cb767a2fee715
Morgan Aubert <morgan@aubert.email>
2020-05-11 20:34:11 -0400

    Ensure app config name cannot be nil or blank

--- src/marten/apps/config.cr
+++ src/marten/apps/config.cr
@@ -7,8 +7,9 @@ module Marten
         @@name = name.to_s
       end
 
-      def self.name
-        @@name
+      def self.name : String
+        raise Errors::InvalidAppConfig.new("An app name must be specified") if @@name.nil? || @@name.not_nil!.empty?
+        @@name.not_nil!
       end
 
       macro inherited
---
+++ src/marten/apps/errors.cr
@@ -0,0 +1,8 @@
+module Marten
+  module Apps
+    module Errors
+      # Represents an error raised when an application config is invalid.
+      class InvalidAppConfig < Exception; end
+    end
+  end
+end

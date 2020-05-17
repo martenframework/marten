cf63dc8347ca05ed5c5525a4402d92fde385f4d9
Morgan Aubert <morgan@aubert.email>
2020-05-16 21:52:00 -0400

    Ensure DB model names are generated using app names by default

--- src/marten/db/model.cr
+++ src/marten/db/model.cr
@@ -1,15 +1,23 @@
 module Marten
   module DB
     abstract class Model
+      @@app_config : Marten::Apps::Config?
+
       def self.table_name
-        @@table_name ||= %{#{name.gsub("::", "_").underscore}s}
+        @@table_name ||= %{#{app_config.name.downcase}_#{name.gsub("::", "_").underscore}s}
       end
 
-      #private def self.app_config
-      #  @@app_config ||= begin
-      #
-      #  end
-      #end
+      private def self.app_config
+        @@app_config ||= begin
+          config = Marten.apps.get_containing_model(self)
+
+          if config.nil?
+            raise Exception.new("Model class is not part of an application defined in Marten.config.installed_apps")
+          end
+
+          config.not_nil!
+        end
+      end
 
       macro inherited
         def self.dir_location

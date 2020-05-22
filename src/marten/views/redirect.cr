feb4b612708177b148f5961e523abd370ba8ef57
Morgan Aubert <morgan@aubert.email>
2020-05-21 22:00:14 -0400

    Add basic redirect view class

---
+++ src/marten/views/redirect.cr
@@ -0,0 +1,36 @@
+module Marten
+  module Views
+    class Redirect < Base
+      class_getter forward_query_string : Bool = false
+      class_getter permanent : Bool = false
+      class_getter route_name : String?
+      class_getter url : String?
+
+      def self.forward_query_string(forward_query_string : Bool)
+        @@forward_query_string = forward_query_string
+      end
+
+      def self.permanent(permanent : Bool)
+        @@permanent = permanent
+      end
+
+      def self.permanent(route_name : String?)
+        @@route_name = route_name
+      end
+
+      def self.url(url : String?)
+        @@url = url
+      end
+
+      def get
+        url = redirect_url
+        return HTTP::Response::Gone.new if url.nil?
+        self.class.permanent ? HTTP::Response::PermanentRedirect.new(url) : HTTP::Response::Redirect.new(url)
+      end
+
+      private def redirect_url
+        self.class.url || (self.class.route_name && reverse(self.class.route_name.not_nil!, params))
+      end
+    end
+  end
+end

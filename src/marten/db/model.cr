3896e6626a360defc2dbbba097e404fd2372fe87
Morgan Aubert <morgan@aubert.email>
2020-06-08 20:54:34 -0400

    Add wip model field macro

--- src/marten/db/model.cr
+++ src/marten/db/model.cr
@@ -20,6 +20,25 @@ module Marten
         QuerySet.new(self)
       end
 
+      macro field(*args, **kwargs)
+        {% if args.size != 2 %}{% raise "A field name and type must be explicitly specified" %}{% end %}
+
+        {% sanitized_id = args[0].is_a?(StringLiteral) || args[0].is_a?(SymbolLiteral) ? args[0].id : nil %}
+        {% if sanitized_id.is_a?(NilLiteral) %}{% raise "Cannot use '#{args[0]}' as a valid field name" %}{% end %}
+
+        {% sanitized_type = args[1].is_a?(StringLiteral) || args[1].is_a?(SymbolLiteral) ? args[1].id : nil %}
+        {% if sanitized_type.is_a?(NilLiteral) %}{% raise "Cannot use '#{args[1]}' as a valid field type" %}{% end %}
+
+        register_field({{ sanitized_id.stringify }}, {{ sanitized_type.stringify }}, **{{ kwargs }})
+
+        def {{ sanitized_id }}
+        end
+      end
+
+      protected def self.register_field(id, type, **options)
+        # Do something...
+      end
+
       private def self.app_config
         @@app_config ||= begin
           config = Marten.apps.get_containing_model(self)

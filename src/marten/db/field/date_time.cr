71c12ae5b1e4b610c90c1e9d7937795c1fbacb5a
Morgan Aubert <morgan@aubert.email>
2020-06-18 21:04:31 -0400

    Ensure date time model field makes use of the configured time zone

--- src/marten/db/field/date_time.cr
+++ src/marten/db/field/date_time.cr
@@ -3,7 +3,7 @@ module Marten
     module Field
       class DateTime < Base
         def from_db_result_set(result_set : ::DB::ResultSet) : Time
-          result_set.read(Time)
+          result_set.read(Time).in(Marten.settings.time_zone)
         end
       end
     end

e80a6990c74af7b4c465e2a6bf27b7fdf0d02a81
Morgan Aubert <morgan@aubert.email>
2020-07-24 17:22:38 -0400

    Ensure base HTTP param abstraction is exposed in the auto doc

--- src/marten/http/params/base.cr
+++ src/marten/http/params/base.cr
@@ -1,7 +1,10 @@
 module Marten
   module HTTP
     module Params
-      # :nodoc:
+      # Represents an abstract set of parameters (query parameters, form data, etc).
+      #
+      # This abstract class is subclassed by `Marten::HTTP::Params::Data` and `Marten::HTTP::Params::Query`, which
+      # respectively provided access to the parsed content of request bodies and request query strings.
       abstract class Base
         # Returns the last value associated with the passed parameter name.
         def [](name : String | Symbol)

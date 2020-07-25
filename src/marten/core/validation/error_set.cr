ebdd9e27b731ad5cbdec5779bdac29177f08ac22
Morgan Aubert <morgan@aubert.email>
2020-07-25 19:44:14 -0400

    Add doc for core error set abstraction

--- src/marten/core/validation/error_set.cr
+++ src/marten/core/validation/error_set.cr
@@ -9,16 +9,46 @@ module Marten
           @errors = [] of Error
         end
 
+        # Adds a new global error to the error set.
+        #
+        # This method can be used to add a new global error (that is not associated with a specific field) to an
+        # existing error set. At least a `message` must be specified:
+        #
+        # ```
+        # record.errors.add("This record is invalid!")
+        # ```
+        #
+        # Optionnaly, an error `type` can be explicitely specified, otherwise it defaults to `:invalid`:
+        #
+        # ```
+        # record.errors.add("This record is invalid!", type: :invalid_record)
+        # ```
         def add(message : String, *, type : Nil | String | Symbol = nil)
           @errors << Error.new(type: type || :invalid, field: nil, message: message)
         end
 
+        # Adds a new field error to the error set.
+        #
+        # This method can be used to add a new error associated with a specific field to an existing error set. At least
+        # a `field` and a `message` must be specified:
+        #
+        # ```
+        # record.errors.add(:attribute, "This record attribute is invalid")
+        # ```
+        #
+        # Optionnaly, an error `type` can be explicitely specified, otherwise it defaults to `:invalid`:
+        #
+        # ```
+        # records.errors.add(:attribute, "This record attribute is invalid!", type: :invalid_attribute)
+        # ```
         def add(field : String | Symbol, message : String, *, type : Nil | String | Symbol = nil)
           @errors << Error.new(type: type || :invalid, field: field, message: message)
         end
 
+        # Iterates over all the `Marten::Core::Validation::Error` objects in this error set.
         delegate each, to: @errors
 
+        # Clears all the `Marten::Core::Validation::Error` objects from this error set.
         delegate clear, to: @errors
       end
     end

15ce1f155c4a7597cacae9f25f4a8cfe50be1343
Morgan Aubert <morgan@aubert.email>
2020-08-04 21:03:48 -0400

    Add new_record? and destroyed? methods to the base model class

--- src/marten/db/model/persistence.cr
+++ src/marten/db/model/persistence.cr
@@ -8,6 +8,22 @@ module Marten
         # :nodoc:
         @new_record : Bool = true
 
+        # Returns a boolean indicating if the record doesn't exist in the database yet.
+        #
+        # This methods returns `true` if the model instance hasn't been saved and doesn't exist in the database yet. In
+        # any other cases it returns `false`.
+        def new_record?
+          @new_record
+        end
+
+        # Returns a boolean indicating if the record was destroyed or not.
+        #
+        # This method returns `true` if the model instance was destroyed previously. Other it returns `false` in any
+        # other cases.
+        def destroyed?
+          @destroyed
+        end
+
         protected setter new_record
       end
     end

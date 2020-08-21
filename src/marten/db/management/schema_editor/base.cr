dd52dd5051f6bbe80ef7d940d968dd8428e330c1
Morgan Aubert <morgan@aubert.email>
2020-08-20 21:11:52 -0400

    Add missing abstractions

---
+++ src/marten/db/management/schema_editor/base.cr
@@ -0,0 +1,35 @@
+module Marten
+  module DB
+    module Management
+      module SchemaEditor
+        abstract class Base
+          def initialize(@connection : Connection::Base)
+          end
+
+          abstract def create_table_statement(table_name : String, column_definitions : String)
+
+          def create_model(model : Model.class)
+            column_definitions = [] of String
+
+            model.fields.each do |field|
+              column_type = column_sql_for_field(field)
+              column_definitions << "#{@connection.quote(field.db_column)} #{column_type}"
+            end
+
+            sql = create_table_statement(@connection.quote(model.table_name), column_definitions.join(", "))
+
+            @connection.open do |db|
+              db.exec(sql)
+            end
+          end
+
+          private def column_sql_for_field(field)
+            sql = field.db_type(@connection)
+            sql += " PRIMARY KEY" if field.primary_key?
+            sql
+          end
+        end
+      end
+    end
+  end
+end

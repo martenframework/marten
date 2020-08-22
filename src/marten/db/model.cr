83572230ebe8d3ae0e8ad604a5008abead185691
Morgan Aubert <morgan@aubert.email>
2020-08-22 17:57:10 -0400

    Add ability to initialize model objects with a block

--- src/marten/db/model.cr
+++ src/marten/db/model.cr
@@ -16,6 +16,15 @@ module Marten
       include Validation
 
       def initialize(**kwargs)
+        initialize_field_values(**kwargs)
+      end
+
+      def initialize(**kwargs, &block)
+        initialize_field_values(**kwargs)
+        yield self
+      end
+
+      private def initialize_field_values(**kwargs)
         values = Hash(String, Field::Any).new
         kwargs.each { |key, v| values[key.to_s] = v }
         assign_field_values(values)

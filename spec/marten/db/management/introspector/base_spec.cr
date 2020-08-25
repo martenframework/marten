38226dc299014ce2240f85bf7b775ac7d413c3df
Morgan Aubert <morgan@aubert.email>
2020-08-24 21:40:41 -0400

    Improve setup flow for DB introspector specs

--- spec/marten/db/management/introspector/base_spec.cr
+++ spec/marten/db/management/introspector/base_spec.cr
@@ -2,10 +2,17 @@ require "./spec_helper"
 
 describe Marten::DB::Management::Introspector::Base do
   describe "#table_names" do
+    around_each do |t|
+      schema_editor = Marten::DB::Management::SchemaEditor.for(Marten::DB::Connection.default)
+      schema_editor.create_model(Marten::DB::Management::Introspector::BaseSpec::TestModel)
+
+      t.run
+
+      schema_editor.delete_model(Marten::DB::Management::Introspector::BaseSpec::TestModel)
+    end
+
     it "returns the table names of the associated database connection" do
       connection = Marten::DB::Connection.default
-      schema_editor = Marten::DB::Management::SchemaEditor.for(connection)
-      schema_editor.create_model(Marten::DB::Management::Introspector::BaseSpec::TestModel)
 
       introspector = Marten::DB::Management::Introspector.for(connection)
 

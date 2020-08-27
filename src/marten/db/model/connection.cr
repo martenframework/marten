144bea278b24fb88b0c9d3969f568ce1cadcf024
Morgan Aubert <morgan@aubert.email>
2020-08-26 21:45:51 -0400

    Add a transaction instance method for models

--- src/marten/db/model/connection.cr
+++ src/marten/db/model/connection.cr
@@ -13,12 +13,25 @@ module Marten
           end
 
           # Allows to run the underlying block in a database transaction.
+          #
+          # A optional database alias can be specified in order to define the database connection to use in the context
+          # of the transaction (otherwise it defaults to the default connection). If the passed database alias doesn't
+          # correspond to any defined connections, a `Marten::DB::Errors::UnknownConnection` error will be raised.
           def transaction(using : Nil | String | Symbol = nil)
             conn = using.nil? ? connection : DB::Connection.get(using.to_s)
             conn.transaction do
               yield
             end
           end
+        end
+
+        # Allows to run the underlying block in a database transaction.
+        #
+        # A optional database alias can be specified in order to define the database connection to use in the context of
+        # the transaction (otherwise it defaults to the default connection). If the passed database alias doesn't
+        # correspond to any defined connections, a `Marten::DB::Errors::UnknownConnection` error will be raised.
+        def transaction(using : Nil | String | Symbol = nil, &block)
+          self.class.transaction(using, &block)
         end
       end
     end

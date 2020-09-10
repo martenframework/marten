4a2bbd19a7390fe380839dd5c1f722420b369c6a
Morgan Aubert <morgan@aubert.email>
2020-09-09 22:25:03 -0400

    Ensure migrations recorder expose associated records

--- src/marten/db/management/migrations/recorder.cr
+++ src/marten/db/management/migrations/recorder.cr
@@ -13,16 +13,24 @@ module Marten
             schema_editor.create_model(Record) unless record_table_exist?
           end
 
+          def applied_migrations
+            record_qs
+          end
+
           private def introspector
             @introspector ||= Introspector.for(@connection).not_nil!
           end
 
-          private def schema_editor
-            @schema_editor ||= SchemaEditor.for(@connection).not_nil!
+          private def record_qs
+            Record.using(@connection.alias)
           end
 
           private def record_table_exist?
             introspector.table_names.includes?(Record.table_name)
+          end
+
+          private def schema_editor
+            @schema_editor ||= SchemaEditor.for(@connection).not_nil!
           end
         end
       end

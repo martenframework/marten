65e211638ff65d3b02fda56085d09f2be952effe
Morgan Aubert <morgan@aubert.email>
2020-09-11 15:51:20 -0400

    Add basic interfaces for defining migrations dependencies and replacements

--- src/marten/db/migration.cr
+++ src/marten/db/migration.cr
@@ -19,6 +19,17 @@ module Marten
 
       @@app_config : Marten::Apps::Config?
 
+      class_getter depends_on = [] of Tuple(String, String)
+      class_getter replaces = [] of Tuple(String, String)
+
+      # Allows to specify the dependencies of the current migration.
+      #
+      # This class method takes an `app_name` and a `migration_name` as argument and adds the corresponding migration
+      # to the list of migration dependencies of the current migration.
+      def self.depends_on(app_name : String | Symbol, migration_name : String | Symbol)
+        @@depends_on << {app_name.to_s, migration_name.to_s}
+      end
+
       # :nodoc:
       def self.dir_location
         __DIR__
@@ -32,6 +43,14 @@ module Marten
       # :nodoc:
       def self.migration_name
         "__base__migration"
+      end
+
+      # Allows to specify the migrations the current migration is replacing.
+      #
+      # This class method takes an `app_name` and a `migration_name` as argument and adds the corresponding migration
+      # to the list of migration replaced by the current migration.
+      def self.replaces(app_name : String | Symbol, migration_name : String | Symbol)
+        @@replaces << {app_name.to_s, migration_name.to_s}
       end
 
       protected def self.app_config

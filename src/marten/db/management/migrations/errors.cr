4c04fdff35b6b8c45c2a0bea13554075d263e167
Morgan Aubert <morgan@aubert.email>
2020-10-04 17:03:48 -0400

    Add missing migrations-related error

--- src/marten/db/management/migrations/errors.cr
+++ src/marten/db/management/migrations/errors.cr
@@ -6,6 +6,9 @@ module Marten
           # Represents an error raised when a circular migration dependency is identified.
           class CircularDependency < Exception; end
 
+          # Represents an error raised when an inexisting migration is requested when running migrations.
+          class MigrationNotFound < Exception; end
+
           # Represents an error raised when an unknown node is requested from a specific migrations node.
           class UnknownNode < Exception; end
         end

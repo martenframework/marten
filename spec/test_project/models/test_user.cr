c31716917a1dfbe0adbf33a038ba9c34b06efad8
Morgan Aubert <morgan@aubert.email>
2020-10-18 15:04:22 -0400

    Add missing unique constraint in test model

--- spec/test_project/models/test_user.cr
+++ spec/test_project/models/test_user.cr
@@ -1,6 +1,6 @@
 class TestUser < Marten::DB::Model
   field :id, :big_auto, primary_key: true
-  field :username, :string, blank: false, null: false, name: "", max_size: 155
+  field :username, :string, blank: false, null: false, name: "", max_size: 155, unique: true
   field :email, :string, blank: false, null: false, name: "", max_size: 254
   field :first_name, :string, blank: false, null: false, name: "", max_size: 150
   field :last_name, :string, blank: false, null: false, name: "", max_size: 150

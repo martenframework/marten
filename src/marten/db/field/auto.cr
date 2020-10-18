4fc1e0f599752f0e15807162c6b7b16cc404b783
Morgan Aubert <morgan@aubert.email>
2020-10-18 17:34:29 -0400

    Ensure auto fields can return Int64 values too

--- src/marten/db/field/auto.cr
+++ src/marten/db/field/auto.cr
@@ -4,6 +4,12 @@ module Marten
       class Auto < Int
         include IsAutoField
 
+        def from_db_result_set(result_set : ::DB::ResultSet) : Int32 | Int64 | Nil
+          # Note: Int64 values are explicitly allowed here (even though they are possibly stored as Int32 at the DB
+          # level) because some databases always persist integers as Int64 values.
+          result_set.read(Int32 | Int64 | Nil)
+        end
+
         def to_column : Migration::Column::Base
           Migration::Column::Auto.new(
             db_column,

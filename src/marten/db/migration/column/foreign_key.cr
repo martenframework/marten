78c176e4acbdfcd7f0e4d421764052d0ecf9ad7f
Morgan Aubert <morgan@aubert.email>
2020-10-20 21:58:11 -0400

    Ensure foreign key columns can be initialized from symbols

--- src/marten/db/migration/column/foreign_key.cr
+++ src/marten/db/migration/column/foreign_key.cr
@@ -5,18 +5,23 @@ module Marten
         class ForeignKey < Base
           include IsBuiltInColumn
 
+          @to_table : ::String
+          @to_column : ::String
+
           getter to_column
           getter to_table
 
           def initialize(
             @name : ::String,
-            @to_table : ::String,
-            @to_column : ::String,
+            to_table : ::String | Symbol,
+            to_column : ::String | Symbol,
             @primary_key = false,
             @null = false,
             @unique = false,
             @index = false
           )
+            @to_table = to_table.to_s
+            @to_column = to_column.to_s
           end
         end
       end

229b52f98b253645a5e19769abf3ceb07275dc14
Morgan Aubert <morgan@aubert.email>
2020-10-31 21:43:26 -0400

    Add missing change to test model

--- spec/test_project/models/post.cr
+++ spec/test_project/models/post.cr
@@ -1,5 +1,6 @@
 class Post < Marten::DB::Model
   field :id, :auto, primary_key: true
   field :author, :foreign_key, to: TestUser
+  field :updated_by, :foreign_key, to: TestUser, null: true, blank: true
   field :title, :string, max_size: 128
 end

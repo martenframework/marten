569b3677afe05c7fdddb70b9deb36296b50c71e8
Morgan Aubert <morgan@aubert.email>
2020-11-30 17:34:46 -0500

    Ensure reverse relation abstraction properly handles one to one fields

--- src/marten/db/reverse_relation.cr
+++ src/marten/db/reverse_relation.cr
@@ -8,13 +8,14 @@ module Marten
         @field ||= @model.get_field(@field_id)
       end
 
+      # Returns `true` if the reverse relation is associated with a one to many field.
       def one_to_many?
         field.is_a?(Field::OneToMany)
       end
 
+      # Returns `true` if the reverse relation is associated with a one to one field.
       def one_to_one?
-        # TODO
-        false
+        field.is_a?(Field::OneToOne)
       end
     end
   end

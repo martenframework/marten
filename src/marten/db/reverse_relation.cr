a3018e148f4272729f6153256477021463282c61
Morgan Aubert <morgan@aubert.email>
2020-11-30 22:10:52 -0500

    Ensure reverse relation expose deletion strategy

--- src/marten/db/reverse_relation.cr
+++ src/marten/db/reverse_relation.cr
@@ -1,11 +1,14 @@
 module Marten
   module DB
     class ReverseRelation
+      @field : Field::Base?
+
       def initialize(@model : Model.class, @field_id : String)
       end
 
-      def field
-        @field ||= @model.get_field(@field_id)
+      # Returns the "on delete" strategy to consider for the considered reverse relation.
+      def on_delete : Deletion::Strategy
+        field.as?(Field::OneToMany).try(&.on_delete).not_nil!
       end
 
       # Returns `true` if the reverse relation is associated with a one to many field.
@@ -16,6 +19,10 @@ module Marten
       # Returns `true` if the reverse relation is associated with a one to one field.
       def one_to_one?
         field.is_a?(Field::OneToOne)
+      end
+
+      private def field
+        @model.get_field(@field_id)
       end
     end
   end

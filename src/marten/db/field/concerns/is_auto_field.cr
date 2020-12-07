1701329a64a65f7c795668932d0ca15bc8b70083
Morgan Aubert <morgan@aubert.email>
2020-12-07 18:49:43 -0500

    Ensure auto fields cannot have null set to true

--- src/marten/db/field/concerns/is_auto_field.cr
+++ src/marten/db/field/concerns/is_auto_field.cr
@@ -8,6 +8,10 @@ module Marten
           {% if kwargs.is_a?(NilLiteral) || kwargs[:primary_key].is_a?(NilLiteral) || !kwargs[:primary_key] %}
             {% raise "Auto fields fields must define 'primary_key: true'" %}
           {% end %}
+
+          {% if !kwargs.is_a?(NilLiteral) && kwargs[:null].is_a?(BoolLiteral) && kwargs[:null] %}
+            {% raise "Auto fields fields cannot set 'null: true'" %}
+          {% end %}
         end
 
         protected def perform_validation(_record : Model); end

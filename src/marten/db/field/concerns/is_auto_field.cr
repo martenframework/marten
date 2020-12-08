b6f300ed33bef0ccf3fccecf98bfcb9ccd48cd16
Morgan Aubert <morgan@aubert.email>
2020-12-07 19:12:05 -0500

    Add specs for auto field

--- src/marten/db/field/concerns/is_auto_field.cr
+++ src/marten/db/field/concerns/is_auto_field.cr
@@ -4,6 +4,9 @@ module Marten
       # :nodoc:
       module IsAutoField
         # :nodoc:
+        def perform_validation(_record : Model); end
+
+        # :nodoc:
         macro check_definition(field_id, kwargs)
           {% if kwargs.is_a?(NilLiteral) || kwargs[:primary_key].is_a?(NilLiteral) || !kwargs[:primary_key] %}
             {% raise "Auto fields fields must define 'primary_key: true'" %}
@@ -13,8 +16,6 @@ module Marten
             {% raise "Auto fields fields cannot set 'null: true'" %}
           {% end %}
         end
-
-        protected def perform_validation(_record : Model); end
       end
     end
   end

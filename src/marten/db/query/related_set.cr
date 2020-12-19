c994c59ae9683d863ef83520065ba7f83b32862a
Morgan Aubert <morgan@aubert.email>
2020-12-18 19:51:26 -0500

    Ensure related query sets mutate the underlying query at initialization time

--- src/marten/db/query/related_set.cr
+++ src/marten/db/query/related_set.cr
@@ -5,16 +5,12 @@ module Marten
     module Query
       class RelatedSet(Model) < Set(Model)
         def initialize(@instance : Marten::DB::Model, @related_field_id : String, @query = SQL::Query(Model).new)
+          @query.add_query_node(Node.new({@related_field_id => @instance.pk}))
         end
 
         protected def clone
           cloned = self.class.new(@instance, @related_field_id, @query.clone)
           cloned
-        end
-
-        protected def fetch
-          @query.add_query_node(Node.new({@related_field_id => @instance.pk}))
-          @result_cache = @query.execute
         end
       end
     end

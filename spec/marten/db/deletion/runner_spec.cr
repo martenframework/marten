6050240325d8900f32a4612f1ccef3f18aef1e1d
Morgan Aubert <morgan@aubert.email>
2020-12-22 10:08:46 -0500

    Add missing spec for deletion runner

--- spec/marten/db/deletion/runner_spec.cr
+++ spec/marten/db/deletion/runner_spec.cr
@@ -125,5 +125,36 @@ describe Marten::DB::Deletion::Runner do
       Post.all.map(&.id).should eq [post_2.id]
       ShowcasedPost.all.map(&.id).should eq [showcased_post_2.id]
     end
+
+    it "is able to process many deletions in the right order" do
+      tag_1 = Tag.create!(name: "coding", is_active: true)
+      tag_2 = Tag.create!(name: "crystal", is_active: true)
+      tag_3 = Tag.create!(name: "ruby", is_active: true)
+
+      user_1 = TestUser.create!(username: "jd1", email: "jd1@example.com", first_name: "John", last_name: "Doe")
+      user_2 = TestUser.create!(username: "jd2", email: "jd2@example.com", first_name: "John", last_name: "Doe")
+
+      post_1 = Post.create!(author: user_1, title: "Post 1")
+      post_2 = Post.create!(author: user_2, title: "Post 2")
+      post_3 = Post.create!(author: user_1, title: "Post 3")
+      post_4 = Post.create!(author: user_2, title: "Post 4")
+
+      ShowcasedPost.create!(post: post_1)
+      ShowcasedPost.create!(post: post_2)
+      ShowcasedPost.create!(post: post_3)
+
+      deletion = Marten::DB::Deletion::Runner.new(Marten::DB::Connection.default)
+      deletion.add(post_1)
+      deletion.add(post_2)
+      deletion.add(user_1)
+      deletion.add(tag_1)
+      deletion.add(tag_3)
+      deletion.execute
+
+      TestUser.all.map(&.id).to_set.should eq [user_2.id].to_set
+      Post.all.map(&.id).should eq [post_4.id]
+      ShowcasedPost.all.size.should eq 0
+      Tag.all.map(&.id).should eq [tag_2.id]
+    end
   end
 end

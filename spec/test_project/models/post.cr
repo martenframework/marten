5dab7999d4ce201c6b0cfead143e065e0d0318d9
Morgan Aubert <morgan@aubert.email>
2020-12-22 13:34:26 -0500

    Update test models

--- spec/test_project/models/post.cr
+++ spec/test_project/models/post.cr
@@ -1,5 +1,5 @@
 class Post < Marten::DB::Model
-  field :id, :auto, primary_key: true
+  field :id, :big_auto, primary_key: true
   field :author, :one_to_many, to: TestUser, on_delete: :cascade
   field :updated_by, :one_to_many, to: TestUser, null: true, blank: true, on_delete: :set_null
   field :title, :string, max_size: 128

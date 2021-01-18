f8c173716f59fcd19316dd4e8b0808eff044c892
Morgan Aubert <morgan@aubert.email>
2021-01-17 21:47:57 -0500

    Add specs for related set abstraction

---
+++ spec/marten/db/query/related_set_spec.cr
@@ -0,0 +1,18 @@
+require "./spec_helper"
+
+describe Marten::DB::Query::RelatedSet do
+  describe "#all" do
+    it "is scoped to the related field target" do
+      user_1 = TestUser.create!(username: "jd1", email: "jd1@example.com", first_name: "John", last_name: "Doe")
+      user_2 = TestUser.create!(username: "jd2", email: "jd2@example.com", first_name: "John", last_name: "Doe")
+
+      post_1 = Post.create!(author: user_1, title: "Post 1")
+      Post.create!(author: user_2, title: "Post 2")
+      post_3 = Post.create!(author: user_1, title: "Post 3")
+
+      qset = Marten::DB::Query::RelatedSet(Post).new(user_1, "author_id")
+
+      qset.all.to_a.should eq [post_1, post_3]
+    end
+  end
+end
--- spec/test_project/models/post.cr
+++ spec/test_project/models/post.cr
@@ -1,6 +1,6 @@
 class Post < Marten::DB::Model
   field :id, :big_auto, primary_key: true
-  field :author, :one_to_many, to: TestUser, on_delete: :cascade
+  field :author, :one_to_many, to: TestUser, related: :posts, on_delete: :cascade
   field :updated_by, :one_to_many, to: TestUser, null: true, blank: true, on_delete: :set_null
   field :title, :string, max_size: 128
 

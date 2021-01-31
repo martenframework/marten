b9f0df41f0867d11ff0cc62816f651262e2516dc
Morgan Aubert <morgan@aubert.email>
2021-01-30 21:44:09 -0500

    Add specs for base predicate class

---
+++ spec/marten/db/query/sql/predicate/base_spec.cr
@@ -0,0 +1,15 @@
+require "./spec_helper"
+
+describe Marten::DB::Query::SQL::Predicate::Base do
+  describe "::predicate_name" do
+    it "allows to configure a predicate name and return its value" do
+      Marten::DB::Query::SQL::Predicate::BaseSpec::Test.predicate_name.should eq "this_is_a_test"
+    end
+  end
+end
+
+module  Marten::DB::Query::SQL::Predicate::BaseSpec
+  class Test < Marten::DB::Query::SQL::Predicate::Base
+    predicate_name "this_is_a_test"
+  end
+end

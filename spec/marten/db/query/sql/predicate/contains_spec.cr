89ae21a58eac09f5aed37061215ef4e86104b226
Morgan Aubert <morgan@aubert.email>
2021-01-31 09:01:41 -0500

    Add specs for contains predicate

---
+++ spec/marten/db/query/sql/predicate/contains_spec.cr
@@ -0,0 +1,22 @@
+require "./spec_helper"
+
+describe Marten::DB::Query::SQL::Predicate::Contains do
+  describe "#to_sql" do
+    it "returns the expected SQL statement" do
+      predicate = Marten::DB::Query::SQL::Predicate::Contains.new(Post.get_field("title"), "foo", "table")
+      {% if env("MARTEN_SPEC_DB_CONNECTION").id == "postgresql" %}
+        predicate.to_sql(Marten::DB::Connection.default).should eq(
+          {"table.title LIKE %s", ["%foo%"]}
+        )
+      {% elsif env("MARTEN_SPEC_DB_CONNECTION").id == "mysql" %}
+        predicate.to_sql(Marten::DB::Connection.default).should eq(
+          {"table.title LIKE BINARY %s", ["%foo%"]}
+        )
+      {% else %}
+        predicate.to_sql(Marten::DB::Connection.default).should eq(
+          {"table.title LIKE %s ESCAPE '\\'", ["%foo%"]}
+        )
+      {% end %}
+    end
+  end
+end

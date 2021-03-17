827576be14a1b030f28b4fb5eff0947f610fbc94
Morgan Aubert <morgan@aubert.email>
2021-03-16 21:44:11 -0400

    Fix duplicated predicate node in query related sets

--- src/marten/db/query/related_set.cr
+++ src/marten/db/query/related_set.cr
@@ -4,8 +4,14 @@ module Marten
   module DB
     module Query
       class RelatedSet(Model) < Set(Model)
-        def initialize(@instance : Marten::DB::Model, @related_field_id : String, @query = SQL::Query(Model).new)
-          @query.add_query_node(Node.new({@related_field_id => @instance.pk}))
+        def initialize(@instance : Marten::DB::Model, @related_field_id : String, query : SQL::Query(Model)? = nil)
+          @query = if query.nil?
+            q = SQL::Query(Model).new
+            q.add_query_node(Node.new({@related_field_id => @instance.pk}))
+            q
+          else
+            query.not_nil!
+          end
         end
 
         protected def clone

24847c0d9ab25294be323f7defe7b681ceed3852
Morgan Aubert <morgan@aubert.email>
2021-03-16 21:47:14 -0400

    Fix QA warnings

--- src/marten/db/query/related_set.cr
+++ src/marten/db/query/related_set.cr
@@ -6,12 +6,12 @@ module Marten
       class RelatedSet(Model) < Set(Model)
         def initialize(@instance : Marten::DB::Model, @related_field_id : String, query : SQL::Query(Model)? = nil)
           @query = if query.nil?
-            q = SQL::Query(Model).new
-            q.add_query_node(Node.new({@related_field_id => @instance.pk}))
-            q
-          else
-            query.not_nil!
-          end
+                     q = SQL::Query(Model).new
+                     q.add_query_node(Node.new({@related_field_id => @instance.pk}))
+                     q
+                   else
+                     query.not_nil!
+                   end
         end
 
         protected def clone

34d70954689e08c71db80f02d9e22b32db1c0fc0
Morgan Aubert <morgan@aubert.email>
2021-04-10 08:19:08 -0400

    Add specs for default filter

---
+++ spec/marten/template/filter/default_spec.cr
@@ -0,0 +1,31 @@
+require "./spec_helper"
+
+describe Marten::Template::Filter::Default do
+  describe "#apply" do
+    it "returns the default value if the initial value is false" do
+      filter = Marten::Template::Filter::Default.new
+      filter.apply(Marten::Template::Value.from(false), Marten::Template::Value.from("default")).should eq "default"
+    end
+
+    it "returns the default value if the initial value is 0" do
+      filter = Marten::Template::Filter::Default.new
+      filter.apply(Marten::Template::Value.from(0), Marten::Template::Value.from("default")).should eq "default"
+    end
+
+    it "returns the default value if the initial value is nil" do
+      filter = Marten::Template::Filter::Default.new
+      filter.apply(Marten::Template::Value.from(nil), Marten::Template::Value.from("default")).should eq "default"
+    end
+
+    it "returns the initial value if it is an empty string" do
+      filter = Marten::Template::Filter::Default.new
+      filter.apply(Marten::Template::Value.from(""), Marten::Template::Value.from("default")).should eq ""
+    end
+
+    it "returns the initial value if it is truthy" do
+      filter = Marten::Template::Filter::Default.new
+      filter.apply(Marten::Template::Value.from(42), Marten::Template::Value.from("default")).should eq 42
+      filter.apply(Marten::Template::Value.from("hello"), Marten::Template::Value.from("default")).should eq "hello"
+    end
+  end
+end

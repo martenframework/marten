c3cbdbf46671aadd10ee534314c93988b599618c
Morgan Aubert <morgan@aubert.email>
2021-04-12 21:25:23 -0400

    Add support for filter expressions parsing

---
+++ spec/marten/template/filter_expression_spec.cr
@@ -0,0 +1,128 @@
+require "./spec_helper"
+
+describe Marten::Template::FilterExpression do
+  describe "::new" do
+    it "initializes a filter expression for a simple variable" do
+      expr = Marten::Template::FilterExpression.new("foo")
+      expr.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "bar"
+    end
+
+    it "initializes a filter expression for a simple variable with a single filter without arguments" do
+      expr = Marten::Template::FilterExpression.new("foo|upcase")
+      expr.resolve(Marten::Template::Context{"foo" => "hello"}).should eq "HELLO"
+    end
+
+    it "initializes a filter expression for a simple variable with a single filter with argument" do
+      expr = Marten::Template::FilterExpression.new("foo|default:bar")
+      expr.resolve(Marten::Template::Context{"foo" => nil, "bar" => 42}).should eq 42
+    end
+
+    it "initializes a filter expression for a simple variable with multiple filters applied" do
+      expr = Marten::Template::FilterExpression.new("foo|default:bar|upcase")
+      expr.resolve(Marten::Template::Context{"foo" => nil, "bar" => "hello"}).should eq "HELLO"
+    end
+
+    it "initializes a filter expression for a simple number literal" do
+      expr_1 = Marten::Template::FilterExpression.new("42")
+      expr_1.resolve(Marten::Template::Context{"foo" => "bar"}).should eq 42
+
+      expr_2 = Marten::Template::FilterExpression.new("42.44")
+      expr_2.resolve(Marten::Template::Context{"foo" => "bar"}).should eq 42.44
+    end
+
+    it "initializes a filter expression for a simple number literal with filters" do
+      expr_1 = Marten::Template::FilterExpression.new("42 | upcase")
+      expr_1.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "42.0"
+
+      expr_2 = Marten::Template::FilterExpression.new("42.44|upcase")
+      expr_2.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "42.44"
+    end
+
+    it "initializes a filter expression for a simple string literal" do
+      expr_1 = Marten::Template::FilterExpression.new(%{"foo"})
+      expr_1.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "foo"
+
+      expr_2 = Marten::Template::FilterExpression.new(%{'foo'})
+      expr_2.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "foo"
+
+      expr_3 = Marten::Template::FilterExpression.new(%{"foo \\"bar\\""})
+      expr_3.resolve(Marten::Template::Context{"foo" => "bar"}).should eq %{foo "bar"}
+
+      expr_4 = Marten::Template::FilterExpression.new(%{'foo \\'bar\\''})
+      expr_4.resolve(Marten::Template::Context{"foo" => "bar"}).should eq %{foo 'bar'}
+    end
+
+    it "initializes a filter expression for a simple string literal with filters" do
+      expr_1 = Marten::Template::FilterExpression.new(%{"foo"|upcase})
+      expr_1.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "FOO"
+
+      expr_2 = Marten::Template::FilterExpression.new(%{'foo' | upcase})
+      expr_2.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "FOO"
+
+      expr_3 = Marten::Template::FilterExpression.new(%{"foo \\"bar\\"" | upcase})
+      expr_3.resolve(Marten::Template::Context{"foo" => "bar"}).should eq %{FOO "BAR"}
+
+      expr_4 = Marten::Template::FilterExpression.new(%{'foo \\'bar\\''|upcase})
+      expr_4.resolve(Marten::Template::Context{"foo" => "bar"}).should eq %{FOO 'BAR'}
+    end
+
+    it "raises if the raw string is not a valid filter expression" do
+      expect_raises(
+        Marten::Template::Errors::InvalidSyntax,
+        "Filter expression contains characters that cannot be parsed properly: test|upcase test|foo bar"
+      ) do
+        Marten::Template::FilterExpression.new(%{test|upcase test|foo bar})
+      end
+    end
+
+    it "raises if the raw string contains an invalid variable" do
+      expect_raises(
+        Marten::Template::Errors::InvalidSyntax,
+        "Filter expression does not contain any variable: |test"
+      ) do
+        Marten::Template::FilterExpression.new("|test")
+      end
+    end
+
+    it "raises if the filter expression ends with unexpected characters" do
+      expect_raises(
+        Marten::Template::Errors::InvalidSyntax,
+        "Filter expression ends with characters that cannot be parsed properly: foo|upcase|+"
+      ) do
+        Marten::Template::FilterExpression.new("foo|upcase|+")
+      end
+    end
+  end
+
+  describe "#resolve" do
+    it "returns the expected value for a simple variable" do
+      expr = Marten::Template::FilterExpression.new("foo")
+      expr.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "bar"
+    end
+
+    it "returns the expected value for a simple variable with a single filter applied" do
+      expr = Marten::Template::FilterExpression.new("foo|upcase")
+      expr.resolve(Marten::Template::Context{"foo" => "bar"}).should eq "BAR"
+    end
+
+    it "returns the expected value for a simple variable with a multiple filters applied" do
+      expr = Marten::Template::FilterExpression.new("foo|default:bar|upcase")
+      expr.resolve(Marten::Template::Context{"foo" => nil, "bar" => "hello"}).should eq "HELLO"
+    end
+
+    it "returns the expected value for a variable with nested lookups" do
+      expr = Marten::Template::FilterExpression.new("foo.user.first_name")
+      expr.resolve(Marten::Template::Context{"foo" => {"user" => {"first_name" => "bar"}}}).should eq "bar"
+    end
+
+    it "returns the expected value for a variable with nested lookups and a single filter applied" do
+      expr = Marten::Template::FilterExpression.new("foo.user.first_name | upcase")
+      expr.resolve(Marten::Template::Context{"foo" => {"user" => {"first_name" => "bar"}}}).should eq "BAR"
+    end
+
+    it "returns the expected value for a variable with nested lookups and multiple filters applied" do
+      expr = Marten::Template::FilterExpression.new("foo.user.first_name|default:'hello' | upcase")
+      expr.resolve(Marten::Template::Context{"foo" => {"user" => {"first_name" => nil}}}).should eq "HELLO"
+    end
+  end
+end
--- src/marten/template/filter_expression.cr
+++ src/marten/template/filter_expression.cr
@@ -5,14 +5,69 @@ module Marten
     # A filter expression will resolve an expression such as "foo.bar|filter1|filter2", which could contain a variable
     # (whose specific attributes are accessed) to which filters are optionally applied.
     class FilterExpression
-      def initialize(@raw_expression : String)
-        # TODO: handle filters.
-        @variable = Variable.new(@raw_expression)
+      def initialize(raw_expression : String)
+        # Parse the raw expression and try to extract both the variable expression (literal or not) and the filters
+        # (with optional arguments) that are successively applied to it.
+        @filters_and_args = [] of Tuple(Filter::Base, Variable?)
+        last_match_end = 0
+        raw_variable = nil
+
+        raw_expression.scan(FILTER_RE).each do |match|
+          match_beginning = match.begin
+          if last_match_end != match_beginning
+            raise Errors::InvalidSyntax.new(
+              "Filter expression contains characters that cannot be parsed properly: #{raw_expression}"
+            )
+          end
+
+          if raw_variable.nil?
+            match_variable = match.named_captures["variable"]
+            if match_variable.nil? || match_variable.try(&.empty?)
+              raise Errors::InvalidSyntax.new("Filter expression does not contain any variable: #{raw_expression}")
+            end
+
+            raw_variable = match_variable
+          else
+            filter_name = match.named_captures["filter_name"]
+            filter_arg = match.named_captures["filter_arg"]
+            @filters_and_args << {Filter.get(filter_name.to_s), filter_arg.nil? ? nil : Variable.new(filter_arg)}
+          end
+
+          last_match_end = match.end
+        end
+
+        if last_match_end != raw_expression.size
+          raise Errors::InvalidSyntax.new(
+            "Filter expression ends with characters that cannot be parsed properly: #{raw_expression}"
+          )
+        end
+
+        @variable = Variable.new(raw_variable.not_nil!)
       end
 
+      # Resolves the filter expression for a given context.
       def resolve(context : Context)
-        @variable.resolve(context)
+        result = @variable.resolve(context)
+
+        @filters_and_args.each do |filter, arg|
+          result = filter.apply(result, arg.nil? ? nil : arg.resolve(context))
+        end
+
+        result
       end
+
+      private VARIABLE_RE = /([\w\.]+|[-+\.]?\d[\d\.e]*)|(?:"[^"\\]*(?:\\.[^"\\]*)*"|'[^'\\]*(?:\\.[^'\\]*)*')/
+      private FILTER_RE   = /
+        ^(?P<variable>#{VARIABLE_RE})|
+        (?:\s*\|\s*
+          (?P<filter_name>\w+)
+          (?:\:
+            (?:
+              (?P<filter_arg>#{VARIABLE_RE})
+            )
+          )?
+        )
+      /x
     end
   end
 end

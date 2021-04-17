11892cb0bf0ab3f93c7daf31ae336064b12ef677
Morgan Aubert <morgan@aubert.email>
2021-04-17 14:37:24 -0400

    Add missing spec for template variable node

--- spec/marten/template/node/variable_spec.cr
+++ spec/marten/template/node/variable_spec.cr
@@ -14,5 +14,18 @@ describe Marten::Template::Node::Variable do
       node_2 = Marten::Template::Node::Variable.new("user.name")
       node_2.render(ctx).should eq "John Doe"
     end
+
+    it "returns the string representation of a variable involving filter lookups" do
+      ctx = Marten::Template::Context{
+        "foo"  => "bar",
+        "user" => {"name" => "John Doe"},
+      }
+
+      node_1 = Marten::Template::Node::Variable.new("foo|upcase")
+      node_1.render(ctx).should eq "BAR"
+
+      node_2 = Marten::Template::Node::Variable.new("user.name|upcase")
+      node_2.render(ctx).should eq "JOHN DOE"
+    end
   end
 end

b92957016b18e5a60022601ee777cd87a4053a47
Morgan Aubert <morgan@aubert.email>
2021-09-16 11:51:09 +0200

    #4 - Ensure migrate and listmigrations commands can be applied to a specific database

--- src/marten/cli/manage/command/list_migrations.cr
+++ src/marten/cli/manage/command/list_migrations.cr
@@ -7,18 +7,34 @@ module Marten
           help "List all database migrations."
 
           @app_label : String?
+          @db : String?
 
           def setup
             on_argument(:app_label, "The name of an application to list migrations for") { |v| @app_label = v }
+
+            on_option_with_arg(
+              :db,
+              arg: "alias",
+              description: "Specify the alias of the database on which migrations will be applied or unapplied " \
+                           "(default to \"default\")"
+            ) do |v|
+              @db = v
+            end
           end
 
           def run
             app_config = @app_label.nil? ? nil : Marten.apps.get(@app_label.not_nil!)
-            reader = Marten::DB::Management::Migrations::Reader.new(Marten::DB::Connection.default)
+
+            reader = Marten::DB::Management::Migrations::Reader.new(
+              Marten::DB::Connection.get(db || Marten::DB::Connection::DEFAULT_CONNECTION_NAME)
+            )
+
             list_migrations(reader, app_config.nil? ? reader.apps_with_migrations : [app_config.not_nil!])
           rescue e : Apps::Errors::AppNotFound
             print_error(e.message)
           end
+
+          private getter db
 
           private def list_migrations(reader, app_configs)
             if app_configs.empty?
--- src/marten/cli/manage/command/migrate.cr
+++ src/marten/cli/manage/command/migrate.cr
@@ -6,24 +6,40 @@ module Marten
           help "Run database migrations."
 
           @app_label : String?
+          @db : String?
           @fake : Bool = false
           @migration : String?
 
           def setup
             on_argument(:app_label, "The name of an application to run migrations for") { |v| @app_label = v }
+
             on_argument(
               :migration,
               "A migration target (name or version) up to which the DB should be migrated. " \
               "Use 'zero' to unapply all the migrations of a specific application"
-            ) { |v| @migration = v }
+            ) do |v|
+              @migration = v
+            end
+
             on_option(:fake, "Set migrations as applied or unapplied without running them") { @fake = true }
+
+            on_option_with_arg(
+              :db,
+              arg: "alias",
+              description: "Specify the alias of the database on which migrations will be applied or unapplied " \
+                           "(default to \"default\")"
+            ) do |v|
+              @db = v
+            end
           end
 
           def run
             app_config = @app_label.nil? ? nil : Marten.apps.get(@app_label.not_nil!)
             migration_name = @migration
 
-            runner = Marten::DB::Management::Migrations::Runner.new(Marten::DB::Connection.default)
+            runner = Marten::DB::Management::Migrations::Runner.new(
+              Marten::DB::Connection.get(db || Marten::DB::Connection::DEFAULT_CONNECTION_NAME)
+            )
 
             if !runner.execution_needed?(app_config, migration_name)
               print("No pending migrations to apply")
@@ -38,6 +54,8 @@ module Marten
           rescue e : Apps::Errors::AppNotFound | DB::Management::Migrations::Errors::MigrationNotFound
             print_error(e.message)
           end
+
+          private getter db
 
           private def process_execution_progress(progress)
             case progress.type

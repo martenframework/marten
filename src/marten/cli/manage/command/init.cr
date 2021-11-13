8f222b68a61786f9632cc28a586a590d50d2c59b
Morgan Aubert <morgan@aubert.email>
2021-11-12 19:57:43 -0500

    Ensure init command supports both app and project structures

--- src/marten/cli/manage/command/init.cr
+++ src/marten/cli/manage/command/init.cr
@@ -5,21 +5,24 @@ module Marten
     class Manage
       module Command
         class Init < Base
+          # :nodoc:
+          TEMPLATE_DIR = "#{__DIR__}/init/templates"
+
           help "Initialize a new Marten project or application structure."
 
-          @@templates = [] of Template.class
+          @@app_templates = [] of Template.class
+          @@project_templates = [] of Template.class
 
           @dir : String?
           @name : String?
           @type : String?
 
-          def self.templates
-            @@templates
-          end
+          class_getter app_templates
+          class_getter project_templates
 
           def setup
             on_argument(:type, "Type of structure to initialize: 'project' or 'app'") { |v| @type = v }
-            on_argument(:name, "Name of the project to initialize") { |v| @name = v }
+            on_argument(:name, "Name of the project or app to initialize") { |v| @name = v }
             on_argument(:dir, "Optional destination directory") { |v| @dir = v }
           end
 
@@ -39,19 +42,19 @@ module Marten
             context.name = @name.not_nil!
             context.dir = (@dir.nil? || @dir.not_nil!.empty?) ? @name.not_nil! : @dir.not_nil!
 
-            self.class.templates.each do |template_klass|
-              tpl = template_klass.new(context)
-              print("  › Creating #{style(tpl.path, mode: :dim)}...", ending: "")
-              tpl.render
-              print(style(" DONE", fore: :light_green, mode: :bold))
-            end
+            create_files(
+              @type == "project" ? self.class.project_templates : self.class.app_templates,
+              context
+            )
           end
 
-          TEMPLATE_DIR = "#{__DIR__}/init/templates"
+          macro template(template_path, destination_path, templates_store_var)
+            {% template_klass = destination_path.split(".").map(&.capitalize).join("") %}
+            {% template_klass = template_klass.split("/").map(&.capitalize).join("") %}
+            {% template_klass = templates_store_var.stringify.capitalize + "_" + template_klass %}
+            {% template_klass = template_klass.id %}
 
-          macro template(template_path, destination_path)
-            {% template_klass = template_path.split(".").map(&.capitalize).join("").id %}
-
+            # :nodoc:
             class {{ template_klass }} < Template
               ECR.def_to_s({{"#{TEMPLATE_DIR.id}/#{template_path.id}"}})
 
@@ -60,21 +63,41 @@ module Marten
               end
             end
 
-            templates << {{ template_klass }}
+            {{ templates_store_var }} << {{ template_klass }}
           end
 
-          template "config_routes.cr.ecr", "config/routes.cr"
-          template "config_settings_base.cr.ecr", "config/settings/base.cr"
-          template "config_settings_development.cr.ecr", "config/settings/development.cr"
-          template "config_settings_production.cr.ecr", "config/settings/production.cr"
-          template "config_settings_test.cr.ecr", "config/settings/test.cr"
+          macro app_template(template_path, destination_path)
+            template({{ template_path }}, {{ destination_path }}, app_templates)
+          end
 
-          template "spec_helper.cr.ecr", "spec/spec_helper.cr"
+          macro project_template(template_path, destination_path)
+            template({{ template_path }}, {{ destination_path }}, project_templates)
+          end
 
-          template "src_project.cr.ecr", "src/project.cr"
-          template "src_server.cr.ecr", "src/server.cr"
+          app_template "app/app.cr.ecr", "app.cr"
+          app_template "app/cli.cr.ecr", "cli.cr"
+          app_template "shared/.gitkeep", "views/.gitkeep"
+          app_template "shared/.gitkeep", "migrations/.gitkeep"
+          app_template "shared/.gitkeep", "models/.gitkeep"
 
-          template "manage.cr.ecr", "manage.cr"
+          project_template "project/config/settings/base.cr.ecr", "config/settings/base.cr"
+          project_template "project/config/settings/development.cr.ecr", "config/settings/development.cr"
+          project_template "project/config/settings/production.cr.ecr", "config/settings/production.cr"
+          project_template "project/config/settings/test.cr.ecr", "config/settings/test.cr"
+          project_template "project/config/routes.cr.ecr", "config/routes.cr"
+          project_template "project/spec/spec_helper.cr.ecr", "spec/spec_helper.cr"
+          project_template "project/src/project.cr.ecr", "src/project.cr"
+          project_template "project/src/server.cr.ecr", "src/server.cr"
+          project_template "project/manage.cr.ecr", "manage.cr"
+
+          private def create_files(templates, context)
+            templates.each do |template_klass|
+              tpl = template_klass.new(context)
+              print("  › Creating #{style(tpl.path, mode: :dim)}...", ending: "")
+              tpl.render
+              print(style(" DONE", fore: :light_green, mode: :bold))
+            end
+          end
         end
       end
     end
---
+++ src/marten/cli/manage/command/init/templates/app/cli.cr.ecr
@@ -0,0 +1,1 @@
+require "./migrations/**"

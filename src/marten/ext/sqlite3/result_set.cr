ff366d1d5e746ee2009e6682a43a3e2c394e73af
Morgan Aubert <morgan@aubert.email>
2022-03-24 17:58:20 -0400

    Fix SQLite3 ext

--- src/marten/ext/sqlite3/result_set.cr
+++ src/marten/ext/sqlite3/result_set.cr
@@ -2,7 +2,11 @@ defined?(::SQLite3::ResultSet) do
   class SQLite3::ResultSet < DB::ResultSet
     def read(t : Time?.class) : Time?
       read(String?).try do |v|
-        Time.parse(v, SQLite3::DATE_FORMAT, location: SQLite3::TIME_ZONE)
+        if v.includes? "."
+          Time.parse v, SQLite3::DATE_FORMAT_SUBSECOND, location: SQLite3::TIME_ZONE
+        else
+          Time.parse v, SQLite3::DATE_FORMAT_SECOND, location: SQLite3::TIME_ZONE
+        end
       rescue Time::Format::Error
         Time.parse(v, "%F", location: SQLite3::TIME_ZONE)
       end

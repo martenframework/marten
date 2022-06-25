e6841c8d28c6d715712fad1942b96de9de7cd081
Morgan Aubert <morgan@aubert.email>
2022-06-25 16:56:11 -0400

    Update SQLite3 result set extension

--- src/marten/ext/sqlite3/result_set.cr
+++ src/marten/ext/sqlite3/result_set.cr
@@ -1,11 +1,11 @@
-class SQLite3::ResultSet
-  __marten_defined?(::SQLite3::ResultSet) do
+__marten_defined?(::SQLite3::ResultSet) do
+  class SQLite3::ResultSet < DB::ResultSet
     def read(t : Time?.class) : Time?
       read(String?).try do |v|
         if v.includes? "."
-          Time.parse v, SQLite3::DATE_FORMAT_SUBSECOND, location: SQLite3::TIME_ZONE
+          Time.parse(v, SQLite3::DATE_FORMAT_SUBSECOND, location: SQLite3::TIME_ZONE)
         else
-          Time.parse v, SQLite3::DATE_FORMAT_SECOND, location: SQLite3::TIME_ZONE
+          Time.parse(v, SQLite3::DATE_FORMAT_SECOND, location: SQLite3::TIME_ZONE)
         end
       rescue Time::Format::Error
         Time.parse(v, "%F", location: SQLite3::TIME_ZONE)

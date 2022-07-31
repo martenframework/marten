811c757ce956f2dfe3dce2b2ebd56e106fdb5680
Morgan Aubert <morgan@aubert.email>
2022-07-31 16:20:15 -0400

    Add missing specs for the routing server handler

---
+++ spec/marten/server/handlers/routing_spec.cr
@@ -0,0 +1,56 @@
+require "./spec_helper"
+
+describe Marten::Server::Handlers::Routing do
+  describe "#call" do
+    it "executes the resolved view and sets the obtained response on the context for a route with no parameters" do
+      handler = Marten::Server::Handlers::Routing.new
+
+      context = HTTP::Server::Context.new(
+        request: ::HTTP::Request.new(
+          method: "GET",
+          resource: "/dummy",
+          headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "FR,en;q=0.5"}
+        ),
+        response: ::HTTP::Server::Response.new(io: IO::Memory.new)
+      )
+
+      handler.call(context)
+
+      context.marten.response.not_nil!.content.should eq "It works!"
+    end
+
+    it "executes the resolved view and sets the obtained response on the context for a route with parameters" do
+      handler = Marten::Server::Handlers::Routing.new
+
+      context = HTTP::Server::Context.new(
+        request: ::HTTP::Request.new(
+          method: "GET",
+          resource: "/dummy/42/and/foobar",
+          headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "FR,en;q=0.5"}
+        ),
+        response: ::HTTP::Server::Response.new(io: IO::Memory.new)
+      )
+
+      handler.call(context)
+
+      context.marten.response.not_nil!.content.should eq "It works!"
+    end
+
+    it "raises Marten::Routing::Errors::NoResolveMatch if the requested route cannot be resolved" do
+      handler = Marten::Server::Handlers::Routing.new
+
+      context = HTTP::Server::Context.new(
+        request: ::HTTP::Request.new(
+          method: "GET",
+          resource: "/unknown/42",
+          headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "FR,en;q=0.5"}
+        ),
+        response: ::HTTP::Server::Response.new(io: IO::Memory.new)
+      )
+
+      expect_raises(Marten::Routing::Errors::NoResolveMatch) do
+        handler.call(context)
+      end
+    end
+  end
+end

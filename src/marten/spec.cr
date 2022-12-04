ec3e817b2ae32bad854287f850e7de24148ec5fa
Morgan Aubert <morgan@aubert.email>
2022-12-03 21:46:57 -0500

    #54 - Ensure collected emails are cleared after each spec when using the development backend

--- src/marten/spec.cr
+++ src/marten/spec.cr
@@ -4,6 +4,12 @@ require "./spec/ext/**"
 
 module Marten
   module Spec
+    def self.clear_collected_emails
+      return unless Marten.settings.emailing.backend.is_a?(Marten::Emailing::Backend::Dev)
+
+      Marten.settings.emailing.backend.as(Marten::Emailing::Backend::Dev).delivered_emails.clear
+    end
+
     def self.flush_databases
       Marten::DB::Connection.registry.values.each do |conn|
         Marten::DB::Management::SchemaEditor.run_for(conn) do |schema_editor|
@@ -29,3 +35,4 @@ end
 Spec.before_suite &->Marten.setup
 Spec.before_suite &->Marten::Spec.setup_databases
 Spec.after_each &->Marten::Spec.flush_databases
+Spec.after_each &->Marten::Spec.clear_collected_emails

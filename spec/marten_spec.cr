93a3afde8ff4b43e33cfd2c99c46625998c07d2c
Morgan Aubert <morgan@aubert.email>
2023-01-14 08:26:35 -0500

    Remove some old deactivated specs

--- spec/marten_spec.cr
+++ spec/marten_spec.cr
@@ -6,28 +6,6 @@ describe Marten do
       Marten.assets.should be_a Marten::Asset::Engine
     end
   end
-
-  %{
-  describe "#start" do
-    it "starts the server using the configured host and port" do
-      MartenSpec.run_server.includes?(
-        "Marten running on http://#{Marten.settings.host}:#{Marten.settings.port}"
-      ).should be_true
-    end
-
-    it "starts the server using specific host and port ARGV arguments" do
-      MartenSpec.run_server(%{Marten.start(args: ["-b 0.0.0.0", "-p 3000"])}).includes?(
-        "Marten running on http://0.0.0.0:3000"
-      ).should be_true
-    end
-
-    it "starts the server using specific host and port arguments" do
-      MartenSpec.run_server(%{Marten.start(host: "0.0.0.0", port: 3000)}).includes?(
-        "Marten running on http://0.0.0.0:3000"
-      ).should be_true
-    end
-  end
-  }
 end
 
 module MartenSpec

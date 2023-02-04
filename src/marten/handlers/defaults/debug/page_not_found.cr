74ca48e604e798c543e55e3419d75ba41d55dda1
Morgan Aubert <morgan@aubert.email>
2023-02-04 14:32:39 -0500

    #71 - Ensure that the welcome page shows a link to the generated authentication if applicable

--- src/marten/handlers/defaults/debug/page_not_found.cr
+++ src/marten/handlers/defaults/debug/page_not_found.cr
@@ -7,6 +7,7 @@ module Marten
         # A "welcome" page is returned by this Handler if the requested path corresponds to the root of the application.
         # Otherwise, a standard "Page not found" response is returned.
         class PageNotFound < Handler
+          @auth_available = false
           @error : Exception? = nil
 
           getter error
@@ -20,11 +21,19 @@ module Marten
             end
           end
 
+          private getter? auth_available
+
           private def render_not_found_page
             HTTP::Response::NotFound.new(ECR.render("#{__DIR__}/templates/page_not_found.html.ecr"))
           end
 
           private def render_welcome_page
+            begin
+              Marten.routes.reverse("auth:sign_up")
+              @auth_available = true
+            rescue Routing::Errors::NoReverseMatch
+            end
+
             HTTP::Response.new(ECR.render("#{__DIR__}/templates/welcome.html.ecr"))
           end
         end

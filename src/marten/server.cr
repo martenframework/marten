b00b9c2305cb7c66e9f5cc0b72de874117ed0d44
Morgan Aubert <morgan@aubert.email>
2023-03-04 09:50:51 -0500

    Revist global HTTP server implementation a bit

--- src/marten/server.cr
+++ src/marten/server.cr
@@ -2,34 +2,37 @@ module Marten
   # Wrapper around the Marten server.
   module Server
     # Returns the addresses on which the server is listening.
-    def self.addresses
-      INSTANCE.addresses.map { |address| "http://#{address}" }
+    def self.addresses : Array(String)
+      instance.addresses.map { |address| "http://#{address}" }
+    end
+
+    # Returns the global HTTP server instance.
+    def self.instance : ::HTTP::Server
+      @@instance ||= ::HTTP::Server.new(
+        [
+          ::HTTP::ErrorHandler.new,
+          Handlers::Logger.new,
+          Handlers::Error.new,
+          Handlers::HostVerification.new,
+          Handlers::Middleware.new,
+          Handlers::Routing.new,
+        ]
+      )
     end
 
     # Setups the server (TCP binding).
-    def self.setup
-      INSTANCE.bind_tcp(Marten.settings.host, Marten.settings.port, Marten.settings.port_reuse)
+    def self.setup : Nil
+      instance.bind_tcp(Marten.settings.host, Marten.settings.port, Marten.settings.port_reuse)
     end
 
     # Starts the server.
-    def self.start
-      INSTANCE.listen
+    def self.start : Nil
+      instance.listen
     end
 
     # Stops the server.
-    def self.stop
-      INSTANCE.close
+    def self.stop : Nil
+      instance.close
     end
-
-    private INSTANCE = ::HTTP::Server.new(
-      [
-        ::HTTP::ErrorHandler.new,
-        Handlers::Logger.new,
-        Handlers::Error.new,
-        Handlers::HostVerification.new,
-        Handlers::Middleware.new,
-        Handlers::Routing.new,
-      ]
-    )
   end
 end

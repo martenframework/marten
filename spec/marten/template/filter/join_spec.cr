49574bf3da34da5029bdb9a54df64be2f798449a
Jonathan Siegel <jonathan@siegel>
2023-03-05 14:53:52 +0900

    Added crystal format and removed extraneous debug output.

--- spec/marten/template/filter/join_spec.cr
+++ spec/marten/template/filter/join_spec.cr
@@ -4,10 +4,10 @@ describe Marten::Template::Filter::Join do
   describe "#apply" do
     it "returns the string joined version of the array of values" do
       filter = Marten::Template::Filter::Join.new
-      filter.apply(Marten::Template::Value.from(["Banana","Orange","Apple"]), 
-                   Marten::Template::Value.from(", ")).should eq "Banana, Orange, Apple"
-      filter.apply(Marten::Template::Value.from([42]), 
-                   Marten::Template::Value.from(", ")).should eq "42"
+      filter.apply(Marten::Template::Value.from(["Banana", "Orange", "Apple"]),
+        Marten::Template::Value.from(", ")).should eq "Banana, Orange, Apple"
+      filter.apply(Marten::Template::Value.from([42]),
+        Marten::Template::Value.from(", ")).should eq "42"
     end
   end
 end

02679a264cd1822ef076604ceda7e8fe8c1715e3
Jonathan Siegel <jonathan@siegel>
2023-03-06 15:55:07 +0900

    Added raised error for unsupported type and except any Enumerable.

--- spec/marten/template/filter/join_spec.cr
+++ spec/marten/template/filter/join_spec.cr
@@ -8,6 +8,10 @@ describe Marten::Template::Filter::Join do
         Marten::Template::Value.from(", ")).should eq "Banana, Orange, Apple"
       filter.apply(Marten::Template::Value.from([42]),
         Marten::Template::Value.from(", ")).should eq "42"
+      expect_raises(Marten::Template::Errors::UnsupportedType) do
+        filter.apply(Marten::Template::Value.from(42),
+                   Marten::Template::Value.from(", "))
+      end
     end
   end
 end

b2612f9b102b99b18f367124eaf3cc4e2954715e
Morgan Aubert <morgan@aubert.email>
2023-03-05 20:11:45 -0500

    Ensure model fields validations are consistent with validation callbacks

--- spec/marten/db/model/validation_spec.cr
+++ spec/marten/db/model/validation_spec.cr
@@ -1,23 +1,36 @@
 require "./spec_helper"
+require "./validation_spec/app"
 
 describe Marten::DB::Model::Validation do
+  with_installed_apps Marten::DB::Model::ValidationSpec::App
+
   describe "#valid?" do
     it "validates all the underlying fields" do
-      tag_1 = Tag.new(is_active: false)
-      tag_1.valid?.should be_false
-      tag_1.errors[0].field.should eq "name"
-      tag_1.errors[0].type.should eq "null"
+      object_1 = Marten::DB::Model::ValidationSpec::Record.new(is_active: false)
+      object_1.valid?.should be_false
+      object_1.errors[0].field.should eq "name"
+      object_1.errors[0].type.should eq "null"
 
-      tag_2 = Tag.new(name: "", is_active: false)
-      tag_2.valid?.should be_false
-      tag_2.errors[0].field.should eq "name"
-      tag_2.errors[0].type.should eq "blank"
+      object_2 = Marten::DB::Model::ValidationSpec::Record.new(name: "", is_active: false)
+      object_2.valid?.should be_false
+      object_2.errors[0].field.should eq "name"
+      object_2.errors[0].type.should eq "blank"
     end
 
     it "uses custom validation rules" do
-      tag = Tag.new(name: "must_be_active", is_active: false)
-      tag.valid?.should be_false
-      tag.errors[0].message.should eq "The tag must be active"
+      object = Marten::DB::Model::ValidationSpec::Record.new(name: "must_be_active", is_active: false)
+      object.valid?.should be_false
+      object.errors[0].message.should eq "The record must be active"
+    end
+
+    it "runs field validations consistently with validation callbacks" do
+      object = Marten::DB::Model::ValidationSpec::Record.new(is_active: false)
+
+      object.valid?.should be_false
+      object.errors[0].field.should eq "name"
+      object.errors[0].type.should eq "null"
+
+      object.before_validation_errors_presence.should eq false
     end
   end
 end
---
+++ spec/marten/db/model/validation_spec/models/record.cr
@@ -0,0 +1,20 @@
+module Marten::DB::Model::ValidationSpec
+  class Record < Marten::Model
+    property before_validation_errors_presence = false
+
+    field :id, :big_int, primary_key: true, auto: true
+    field :name, :string, blank: false, null: false, max_size: 64, unique: true
+    field :is_active, :bool, null: false
+
+    before_validation :set_before_validation_errors_presence
+    validate :validate_crystal_is_active
+
+    def set_before_validation_errors_presence
+      self.before_validation_errors_presence = !errors.empty?
+    end
+
+    private def validate_crystal_is_active
+      errors.add("The record must be active") if name == "must_be_active" && !is_active
+    end
+  end
+end

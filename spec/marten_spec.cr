b5828d84a8c65892b203a42b23f8211a8033e05b
Morgan Aubert <morgan@aubert.email>
2023-03-26 09:33:22 -0400

    Remove unnecessary spec helper

--- spec/marten_spec.cr
+++ spec/marten_spec.cr
@@ -7,21 +7,3 @@ describe Marten do
     end
   end
 end
-
-module MartenSpec
-  def self.run_server(code = "Marten.start")
-    full_code = <<-CR
-      require "./src/marten"
-      #{code}
-    CR
-
-    stdout = IO::Memory.new
-    stderr = IO::Memory.new
-
-    process = Process.new("crystal", ["eval"], input: IO::Memory.new(full_code), output: stdout, error: stderr)
-    sleep 5
-    process.signal(Signal::INT)
-
-    stdout.rewind.to_s
-  end
-end

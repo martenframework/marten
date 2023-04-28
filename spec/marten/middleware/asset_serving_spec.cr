4a18651f2c4b8cf4e174ff764b3e1505c4947f24
Morgan Aubert <morgan@aubert.email>
2023-04-27 21:25:56 -0400

    #90 - Add middlweware to serve assets

---
+++ spec/marten/middleware/asset_serving_spec.cr
@@ -0,0 +1,258 @@
+require "./spec_helper"
+
+describe Marten::Middleware::AssetServing do
+  around_each do |t|
+    with_overridden_setting("assets.url", "/assets/") do
+      with_overridden_setting("assets.root", File.join(__DIR__, "asset_serving/assets")) do
+        t.run
+      end
+    end
+  end
+
+  describe "#call" do
+    it "returns the next response early if the request path does not match the configured assets URL" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      response_1 = middleware.call(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/",
+            headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "fr,en;q=0.5"}
+          )
+        ),
+        ->{ Marten::HTTP::Response.new("It works!", content_type: "text/plain", status: 200) }
+      )
+      response_1.content.should eq "It works!"
+
+      response_2 = middleware.call(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/foo/bar",
+            headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "fr,en;q=0.5"}
+          )
+        ),
+        ->{ Marten::HTTP::Response.new("It works!", content_type: "text/plain", status: 200) }
+      )
+      response_2.content.should eq "It works!"
+    end
+
+    it "returns the next response early if the asset path does not exist" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      response_1 = middleware.call(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/assets/unknown.css",
+            headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "fr,en;q=0.5"}
+          )
+        ),
+        ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+      )
+      response_1.content.should eq "Unknown!"
+
+      response_2 = middleware.call(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/assets/css/unknown.css",
+            headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "fr,en;q=0.5"}
+          )
+        ),
+        ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+      )
+      response_2.content.should eq "Unknown!"
+    end
+
+    it "returns the next response early if a path traversal is attempted" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      response = middleware.call(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/assets/../../asset_serving_spec.cr",
+            headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "fr,en;q=0.5"}
+          )
+        ),
+        ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+      )
+      response.content.should eq "Unknown!"
+    end
+
+    it "returns the next response early if the specified path is a directory" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      response = middleware.call(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/assets/css",
+            headers: HTTP::Headers{"Host" => "example.com", "Accept-Language" => "fr,en;q=0.5"}
+          )
+        ),
+        ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+      )
+      response.content.should eq "Unknown!"
+    end
+
+    it "does not compress small asset files" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      ["SmallApp.js", "css/SmallApp.css"].each do |path|
+        response = middleware.call(
+          Marten::HTTP::Request.new(
+            ::HTTP::Request.new(
+              method: "GET",
+              resource: "/assets/#{path}",
+              headers: HTTP::Headers{
+                "Host"            => "example.com",
+                "Accept-Encoding" => "gzip, deflate, br",
+              }
+            )
+          ),
+          ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+        )
+
+        response.headers[:CONTENT_ENCODING]?.should be_nil
+        response.content.should eq File.read(File.join(__DIR__, "asset_serving/assets/#{path}"))
+      end
+    end
+
+    it "does not compress if GZip and deflate are not supported" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      ["BigApp.js", "css/BigApp.css"].each do |path|
+        response = middleware.call(
+          Marten::HTTP::Request.new(
+            ::HTTP::Request.new(
+              method: "GET",
+              resource: "/assets/#{path}",
+              headers: HTTP::Headers{
+                "Host" => "example.com",
+              }
+            )
+          ),
+          ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+        )
+
+        response.headers[:CONTENT_ENCODING]?.should be_nil
+        response.content.should eq File.read(File.join(__DIR__, "asset_serving/assets/#{path}"))
+      end
+    end
+
+    it "compresses using GZip over other supported encoding" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      ["BigApp.js", "css/BigApp.css"].each do |path|
+        response = middleware.call(
+          Marten::HTTP::Request.new(
+            ::HTTP::Request.new(
+              method: "GET",
+              resource: "/assets/#{path}",
+              headers: HTTP::Headers{
+                "Host"            => "example.com",
+                "Accept-Encoding" => "gzip, deflate, br",
+              }
+            )
+          ),
+          ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+        )
+
+        response.headers[:CONTENT_ENCODING].should eq "gzip"
+
+        uncompressed_content = File.read(File.join(__DIR__, "asset_serving/assets/#{path}"))
+        response.content.should_not eq uncompressed_content
+
+        Compress::Gzip::Reader.open(IO::Memory.new(response.content)) do |gzip|
+          gzip.gets_to_end.should eq uncompressed_content
+        end
+      end
+    end
+
+    it "fallbacks to using deflate if GZip is not supported" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      ["BigApp.js", "css/BigApp.css"].each do |path|
+        response = middleware.call(
+          Marten::HTTP::Request.new(
+            ::HTTP::Request.new(
+              method: "GET",
+              resource: "/assets/#{path}",
+              headers: HTTP::Headers{
+                "Host"            => "example.com",
+                "Accept-Encoding" => "deflate, br",
+              }
+            )
+          ),
+          ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+        )
+
+        response.headers[:CONTENT_ENCODING].should eq "deflate"
+
+        uncompressed_content = File.read(File.join(__DIR__, "asset_serving/assets/#{path}"))
+        response.content.should_not eq uncompressed_content
+
+        Compress::Deflate::Reader.open(IO::Memory.new(response.content)) do |deflate|
+          deflate.gets_to_end.should eq uncompressed_content
+        end
+      end
+    end
+
+    it "properly sets the Content-Length header" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      ["BigApp.js", "css/BigApp.css"].each do |path|
+        response = middleware.call(
+          Marten::HTTP::Request.new(
+            ::HTTP::Request.new(
+              method: "GET",
+              resource: "/assets/#{path}",
+              headers: HTTP::Headers{
+                "Host"            => "example.com",
+                "Accept-Encoding" => "gzip, deflate, br",
+              }
+            )
+          ),
+          ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+        )
+
+        response.headers[:CONTENT_ENCODING].should eq "gzip"
+
+        uncompressed_content = File.read(File.join(__DIR__, "asset_serving/assets/#{path}"))
+        response.content.should_not eq uncompressed_content
+        response.headers[:CONTENT_LENGTH].should eq response.content.bytesize.to_s
+      end
+    end
+
+    it "properly sets the ETag header using the file modification time" do
+      middleware = Marten::Middleware::AssetServing.new
+
+      ["BigApp.js", "css/BigApp.css"].each do |path|
+        response = middleware.call(
+          Marten::HTTP::Request.new(
+            ::HTTP::Request.new(
+              method: "GET",
+              resource: "/assets/#{path}",
+              headers: HTTP::Headers{
+                "Host"            => "example.com",
+                "Accept-Encoding" => "gzip, deflate, br",
+              }
+            )
+          ),
+          ->{ Marten::HTTP::Response.new("Unknown!", content_type: "text/plain", status: 200) }
+        )
+
+        response.headers[:CONTENT_ENCODING].should eq "gzip"
+
+        uncompressed_content = File.read(File.join(__DIR__, "asset_serving/assets/#{path}"))
+        response.content.should_not eq uncompressed_content
+        response.headers[:"ETag"].should eq(
+          %{W/"#{File.info(File.join(__DIR__, "asset_serving/assets/#{path}")).modification_time.to_unix}"}
+        )
+      end
+    end
+  end
+end
---
+++ src/marten/middleware/asset_serving.cr
@@ -0,0 +1,96 @@
+module Marten
+  abstract class Middleware
+    # Allows serving compressed static assets.
+    #
+    # This middleware serves the collected assets that are available under the configured assets root (`assets.root`
+    # setting). This assumes that the assets under this folder have been "collected" by using the `collectassets`
+    # management command and that the assets storage being used is the "file system" one
+    # (`Marten::Core::Storage::FileSystem`).
+    #
+    # It should also be noted that this middleware will automatically compress served assets using GZip or deflate based
+    # on the incoming Accept-Encoding header.
+    class AssetServing < Middleware
+      def call(request : Marten::HTTP::Request, get_response : Proc(Marten::HTTP::Response)) : Marten::HTTP::Response
+        return get_response.call if !request.path.starts_with?(Marten.settings.assets.url)
+
+        asset_file_path = File.expand_path(
+          File.join(
+            expanded_assets_path,
+            File.expand_path(request.path, "/").lchop(Marten.settings.assets.url)
+          ),
+          "/"
+        )
+
+        if asset_file_path.starts_with?(expanded_assets_path) && File.exists?(asset_file_path) &&
+           !File.directory?(asset_file_path)
+          serve_asset_file(request, asset_file_path)
+        else
+          get_response.call
+        end
+      end
+
+      private DEFLATE_ACCEPT_ENCODING_RE = /\bdeflate\b/i
+      private GZIP_ACCEPT_ENCODING_RE    = /\bgzip\b/i
+      private SMALL_FILE_SIZE_THRESHOLD  = 200
+
+      private def accepts_deflate_encoding?(request : HTTP::Request) : Bool
+        request.headers.fetch(:ACCEPT_ENCODING, "").matches?(DEFLATE_ACCEPT_ENCODING_RE)
+      end
+
+      private def accepts_gzip_encoding?(request : HTTP::Request) : Bool
+        request.headers.fetch(:ACCEPT_ENCODING, "").matches?(GZIP_ACCEPT_ENCODING_RE)
+      end
+
+      private def expanded_assets_path : String
+        @expanded_assets_path ||= Path[Marten.settings.assets.root].expand.to_s
+      end
+
+      private def compress_using_deflate(file : File, io : IO) : Nil
+        Compress::Deflate::Writer.open(io) do |deflate|
+          IO.copy(file, deflate)
+        end
+      end
+
+      private def compress_using_gzip(file : File, io : IO) : Nil
+        Compress::Gzip::Writer.open(io) do |gzip|
+          IO.copy(file, gzip)
+        end
+      end
+
+      private def serve_asset_file(request : HTTP::Request, asset_file_path : String) : Marten::HTTP::Response
+        asset_file_size = File.size(asset_file_path)
+
+        content_type = begin
+          MIME.from_filename(asset_file_path)
+        rescue KeyError
+          "application/octet-stream"
+        end
+
+        content_encoding = nil
+
+        served_asset_io = IO::Memory.new
+
+        File.open(asset_file_path) do |asset_file|
+          if accepts_gzip_encoding?(request) && asset_file_size >= SMALL_FILE_SIZE_THRESHOLD
+            compress_using_gzip(asset_file, served_asset_io)
+            content_encoding = "gzip"
+          elsif accepts_deflate_encoding?(request) && asset_file_size >= SMALL_FILE_SIZE_THRESHOLD
+            compress_using_deflate(asset_file, served_asset_io)
+            content_encoding = "deflate"
+          else
+            IO.copy(asset_file, served_asset_io)
+          end
+        end
+
+        response = HTTP::Response.new(content: served_asset_io.rewind.gets_to_end, content_type: content_type)
+        served_asset_io.close
+
+        response.headers[:"Content-Length"] = response.content.bytesize
+        response.headers[:"ETag"] = %{W/"#{File.info(asset_file_path).modification_time.to_unix}"}
+        response.headers[:"Content-Encoding"] = content_encoding if !content_encoding.nil?
+
+        response
+      end
+    end
+  end
+end

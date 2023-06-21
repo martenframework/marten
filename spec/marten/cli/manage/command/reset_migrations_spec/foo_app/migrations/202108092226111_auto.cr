4ebef0f7a27c93ca9325aeb844f5e5f489c6331b
Morgan Aubert <morgan@aubert.email>
2023-06-21 19:42:00 -0400

    Fix possible flaky test in CLI specs

--- spec/marten/cli/manage/command/reset_migrations_spec/foo_app/migrations/202108092226111_auto.cr
+++ spec/marten/cli/manage/command/reset_migrations_spec/foo_app/migrations/202108092226111_auto.cr
@@ -1,6 +1,6 @@
 class Migration::ResetMigrationsSpec::FooApp::V202108092226111 < Marten::Migration
   def plan
-    create_table :foo_tags do
+    create_table :cli_manage_command_migrate_spec_foo_tags do
       column :id, :big_int, primary_key: true, auto: true
       column :label, :string, max_size: 255, unique: true
     end
--- spec/marten/cli/manage/command/reset_migrations_spec/foo_app/migrations/202108092226112_auto.cr
+++ spec/marten/cli/manage/command/reset_migrations_spec/foo_app/migrations/202108092226112_auto.cr
@@ -2,6 +2,6 @@ class Migration::ResetMigrationsSpec::FooApp::V202108092226112 < Marten::Migrati
   depends_on "reset_migrations_spec_foo_app", "202108092226111_auto"
 
   def plan
-    add_column :foo_tags, :active, :bool, default: true
+    add_column :cli_manage_command_migrate_spec_foo_tags, :active, :bool, default: true
   end
 end

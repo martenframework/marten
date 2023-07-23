a40c959794969479080633b335df76bbd9621def
Morgan Aubert <morgan@aubert.email>
2023-07-23 08:34:55 -0400

    Simplify many-to-many field specs

--- spec/marten/db/field/many_to_many_spec.cr
+++ spec/marten/db/field/many_to_many_spec.cr
@@ -6,17 +6,32 @@ describe Marten::DB::Field::ManyToMany do
 
   describe "::contribute_to_model" do
     it "works as expected for non-recursive many-to-many relationships" do
-      article = Marten::DB::Field::ManyToManySpec::Article.create!(title: "My article")
-      tag_1 = Marten::DB::Field::ManyToManySpec::Tag.create!(label: "Tag 1")
-      tag_2 = Marten::DB::Field::ManyToManySpec::Tag.create!(label: "Tag 2")
+      user_1 = TestUser.create!(username: "jd1", email: "jd1@example.com", first_name: "John", last_name: "Doe")
+      user_2 = TestUser.create!(username: "jd2", email: "jd2@example.com", first_name: "John", last_name: "Doe")
 
-      article.tags.add(tag_1, tag_2)
+      tag_1 = Tag.create!(name: "coding", is_active: true)
+      tag_2 = Tag.create!(name: "crystal", is_active: true)
+      tag_3 = Tag.create!(name: "ruby", is_active: true)
 
-      article = Marten::DB::Field::ManyToManySpec::Article.get!(id: article.id)
-      article.tags.to_a.to_set.should eq [tag_1, tag_2].to_set
+      user_1.tags.add(tag_1)
+      user_1.tags.add(tag_2)
+      user_2.tags.add(tag_1)
+      user_2.tags.add(tag_3)
 
-      tag_1 = Marten::DB::Field::ManyToManySpec::Tag.get!(id: tag_1.id)
-      tag_1.articles.to_a.should eq [article]
+      user_1 = TestUser.get!(id: user_1.id)
+      user_1.tags.to_a.to_set.should eq [tag_1, tag_2].to_set
+
+      user_2 = TestUser.get!(id: user_2.id)
+      user_2.tags.to_a.to_set.should eq [tag_1, tag_3].to_set
+
+      tag_1 = Tag.get!(id: tag_1.id)
+      tag_1.test_users.to_a.to_set.should eq [user_1, user_2].to_set
+
+      tag_2 = Tag.get!(id: tag_2.id)
+      tag_2.test_users.to_a.should eq [user_1]
+
+      tag_3 = Tag.get!(id: tag_3.id)
+      tag_3.test_users.to_a.should eq [user_2]
     end
 
     it "works as expected for recursive many-to-many relationships" do
@@ -31,24 +46,6 @@ describe Marten::DB::Field::ManyToMany do
 
       child_node_1 = Marten::DB::Field::ManyToManySpec::TreeNode.get!(id: child_node_1.id)
       child_node_1.parents.to_a.should eq [parent_node]
-    end
-
-    it "sets up a reverse queryset method when the related option is used" do
-      user_1 = TestUser.create!(username: "jd1", email: "jd1@example.com", first_name: "John", last_name: "Doe")
-      user_2 = TestUser.create!(username: "jd2", email: "jd2@example.com", first_name: "John", last_name: "Doe")
-
-      tag_1 = Tag.create!(name: "coding", is_active: true)
-      tag_2 = Tag.create!(name: "crystal", is_active: true)
-      tag_3 = Tag.create!(name: "ruby", is_active: true)
-
-      user_1.tags.add(tag_1)
-      user_1.tags.add(tag_2)
-      user_2.tags.add(tag_1)
-      user_2.tags.add(tag_3)
-
-      tag_1.test_users.to_a.to_set.should eq [user_1, user_2].to_set
-      tag_2.test_users.to_a.should eq [user_1]
-      tag_3.test_users.to_a.should eq [user_2]
     end
   end
 
--- spec/marten/db/field/many_to_many_spec/models/article.cr
+++
@@ -1,7 +0,0 @@
-module Marten::DB::Field::ManyToManySpec
-  class Article < Marten::Model
-    field :id, :big_int, primary_key: true, auto: true
-    field :title, :string, max_size: 255
-    field :tags, :many_to_many, to: Marten::DB::Field::ManyToManySpec::Tag, related: :articles
-  end
-end

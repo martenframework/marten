7e10f8744dab99ec463ed0540257ed754218d87d
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2023-08-03 17:41:19 +0200

    Formatting

--- src/marten/core/validator/slug.cr
+++ src/marten/core/validator/slug.cr
@@ -1,17 +1,17 @@
 module Marten
-    module Core
-      module Validator
-        # Slug validator.
-        module Slug
-          extend self
+  module Core
+    module Validator
+      # Slug validator.
+      module Slug
+        extend self
 
-          # Returns `true` if the passed string corresponds to a valid slug.
-          def self.valid?(value : String) : Bool
-            !!value.match(SLUG_RE)
-          end
-
-          private SLUG_RE = /^[a-z0-9]+(?:(?:-|_)[a-z0-9]+)*$/
+        # Returns `true` if the passed string corresponds to a valid slug.
+        def self.valid?(value : String) : Bool
+          !!value.match(SLUG_RE)
         end
+
+        private SLUG_RE = /^[a-z0-9]+(?:(?:-|_)[a-z0-9]+)*$/
       end
     end
   end
+end

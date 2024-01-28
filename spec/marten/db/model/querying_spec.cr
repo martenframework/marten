d668c7f3da25f5244e31f096c736f43098312d99
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2024-01-28 21:15:48 +0100

    Add spec

--- spec/marten/db/model/querying_spec.cr
+++ spec/marten/db/model/querying_spec.cr
@@ -43,6 +43,16 @@ describe Marten::DB::Model::Querying do
     end
   end
 
+  describe "::average" do
+    it "properly calculates the average" do
+      user = TestUser.create!(username: "jd1", email: "jd1@example.com", first_name: "John", last_name: "Doe")
+      Post.create!(author: user, title: "Example post 1", score: 5.0)
+      Post.create!(author: user, title: "Example post 2", score: 5.0)
+
+      Post.average(:score).should be_close(5.0, 0.00001)
+    end
+  end
+
   describe "::default_queryset" do
     it "returns a queryset containing all the objects by default" do
       TestUser.create!(username: "jd1", email: "jd@example.com", first_name: "John", last_name: "Doe")

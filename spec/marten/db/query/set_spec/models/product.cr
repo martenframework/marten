d2181c346f239581ce9f0207ab5da70502163d06
Morgan Aubert <morgan@aubert.email>
2024-02-02 23:00:35 -0500

    Remove unnecessary spec model

--- spec/marten/db/query/set_spec/models/product.cr
+++
@@ -1,8 +0,0 @@
-module Marten::DB::Query::SetSpec
-  class Product < Marten::Model
-    field :id, :big_int, primary_key: true, auto: true
-    field :name, :string, max_size: 255
-    field :price, :int
-    field :rating, :float, blank: true, null: true
-  end
-end

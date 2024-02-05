b84fb64780b4a5fdb71db60f2a8e387a63d5c756
Marvin Ahlgrimm <treagod@users.noreply.github.com>
2024-02-06 00:40:24 +0100

    Removes duplicated assignment (#154)

--- src/marten/db/query/sql/query.cr
+++ src/marten/db/query/sql/query.cr
@@ -166,8 +166,6 @@ module Marten
           end
 
           def setup_distinct_clause(fields : Array(String) | Nil = nil) : Nil
-            self.distinct = true
-
             distinct_columns = [] of String
 
             if !fields.nil?

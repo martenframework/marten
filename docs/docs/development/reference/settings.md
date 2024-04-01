6b141148adc687fe7a06cfd8042762e43d5789d3
Marvin Ahlgrimm <treagod@users.noreply.github.com>
2024-04-01 05:48:30 +0200

    Override template loaders (#216)
    Adds the option to manually define the used template loaders.

--- docs/docs/development/reference/settings.md
+++ docs/docs/development/reference/settings.md
@@ -769,6 +769,16 @@ Default: `false`
 
 A boolean option enabling or disabling isolated inclusions of templates when using the [`include`](../../templates/reference/tags.md#include) template tag. When set to `true`, included templates won't have access to variables from the outer context by default, unless the `contextual` modifier is used with the `include` template tag. Conversely, when set to `false`, included templates will have access to the outer context's variables by default, unless explicitly disabled with the `isolated` modifier of the `include` template tag.
 
+### `loaders`
+
+Default: `nil`
+
+Overwrites the loading mechanism of templates. Takes an array of classes inheriting from `Marten::Template::Loader::Base`. Customize this to load templates from various sources like databases or in-memory structures. For example, to load templates from a file system:
+
+```crystal
+config.templates.loaders = [Marten::Template::Loader::FileSystem.new("/path/to/templates")] of Marten::Template::Loader::Base
+```
+
 ### `strict_variables`
 
 Default: `false`
--- spec/marten_spec.cr
+++ spec/marten_spec.cr
@@ -30,4 +30,37 @@ describe Marten do
       end
     end
   end
+
+  describe "#setup_templates" do
+    after_each do
+      Marten.setup_templates
+    end
+
+    it "setup templates uses no loaders when `templates.loaders` setting is empty array" do
+      with_overridden_setting("templates.loaders", [] of Marten::Template::Loader::Base, nilable: true) do
+        Marten.setup_templates
+        Marten.templates.loaders.size.should eq 0
+      end
+    end
+
+    it "setup templates uses configured loaders" do
+      file_system_loader = Marten::Template::Loader::FileSystem.new("/root/templates")
+      with_overridden_setting(
+        "templates.loaders",
+        [file_system_loader] of Marten::Template::Loader::Base,
+        nilable: true
+      ) do
+        Marten.setup_templates
+        Marten.templates.loaders.size.should eq 1
+        Marten.templates.loaders[0].should eq file_system_loader
+      end
+    end
+
+    it "setup templates uses default loaders when setting is nil" do
+      with_overridden_setting("templates.loaders", nil, nilable: true) do
+        Marten.setup_templates
+        Marten.templates.loaders.size.should eq 2
+      end
+    end
+  end
 end

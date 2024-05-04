d686b40bc1d79b3baa0ce075b7f229a052fa46d5
Morgan Aubert <morgan@aubert.email>
2024-05-04 12:33:36 -0400

    Make it possible to assign the output of the csrf_token template tag to a variable

--- src/marten/template/tag/csrf_token.cr
+++ src/marten/template/tag/csrf_token.cr
@@ -1,3 +1,5 @@
+require "./concerns/*"
+
 module Marten
   module Template
     module Tag
@@ -6,15 +8,41 @@ module Marten
       # The `csrf_token` template tag allows to compute and insert the value of the CSRF token into a template. This
       # tag requires the presence of a handler object in the template context (under the `"handler"` key), otherwise an
       # empty token is returned.
+      #
+      # Optionally, the output of this tag can be assigned to a specific variable using the `as` keyword:
+      #
+      # ```
+      # {% csrf_token as my_var %}
+      # ```
       class CsrfToken < Base
-        def initialize(_parser : Parser, _source : String)
+        include CanSplitSmartly
+
+        @assigned_to : String? = nil
+
+        def initialize(_parser : Parser, source : String)
+          parts = split_smartly(source)
+
+          if parts.size == 2 || parts.size > 3
+            raise Errors::InvalidSyntax.new("Malformed csrf_token tag: either no arguments or two arguments expected")
+          end
+
+          if parts.size == 3 && parts[1] == "as"
+            @assigned_to = parts[2]
+          elsif parts.size == 3
+            raise Errors::InvalidSyntax.new("Malformed csrf_token tag: 'as' keyword expected")
+          end
         end
 
         def render(context : Context) : String
           handler = context["handler"]?.try(&.raw).as?(Handlers::Base)
           return "" if handler.nil?
 
-          handler.get_csrf_token
+          if @assigned_to.nil?
+            handler.get_csrf_token
+          else
+            context[@assigned_to.not_nil!] = handler.get_csrf_token
+            ""
+          end
         end
       end
     end

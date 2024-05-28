ec7d61196844f48dcf974431a9afa9e42c6d0004
Morgan Aubert <morgan@aubert.email>
2024-05-27 20:31:59 -0400

    Ensure csrf_input tag supports assignments

--- spec/marten/template/tag/csrf_input_spec.cr
+++ spec/marten/template/tag/csrf_input_spec.cr
@@ -2,20 +2,7 @@ require "./spec_helper"
 
 describe Marten::Template::Tag::CsrfInput do
   describe "::new" do
-    it "raises if the csrf_input tag contains an argument" do
-      parser = Marten::Template::Parser.new("")
-
-      expect_raises(
-        Marten::Template::Errors::InvalidSyntax,
-        "Malformed csrf_input tag: takes no argument"
-      ) do
-        Marten::Template::Tag::CsrfInput.new(parser, %(csrf_input "arg"))
-      end
-    end
-  end
-
-  describe "#render" do
-    it "renders a html hidden input tag with csrftoken as name and handler's csrf_token as value" do
+    it "can initialize a CSRF token tag without assignment" do
       parser = Marten::Template::Parser.new("")
 
       tag = Marten::Template::Tag::CsrfInput.new(parser, "csrf_input")
@@ -33,9 +20,123 @@ describe Marten::Template::Tag::CsrfInput do
       handler.process_dispatch
 
       context = Marten::Template::Context.new
-      context["handler"] = handler
+      context.handler = handler
 
-      tag.render(context).includes?(%(<input type="hidden" name="csrftoken" value="#{handler.get_csrf_token}" />))
+      tag.render(context).should_not be_empty
+    end
+
+    it "can initialize a CSRF token tag with an assignment" do
+      parser = Marten::Template::Parser.new("")
+
+      tag = Marten::Template::Tag::CsrfInput.new(parser, "csrf_input as my_var")
+      tag.should be_a Marten::Template::Tag::CsrfInput
+
+      handler = Marten::Template::Tag::CsrfInputSpec::TestHandler.new(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/test/xyz",
+            headers: HTTP::Headers{"Host" => "example.com"}
+          )
+        )
+      )
+      handler.process_dispatch
+
+      context = Marten::Template::Context.new
+      context.handler = handler
+
+      tag.render(context).should be_empty
+      context["my_var"].should_not be_empty
+    end
+
+    it "raises if the CSRF token tag's syntax is invalid" do
+      ["csrf_input as", "csrf_input as my_var other_var"].each do |raw_tag|
+        expect_raises(
+          Marten::Template::Errors::InvalidSyntax,
+          "Malformed csrf_input tag: either no arguments or two arguments expected"
+        ) do
+          Marten::Template::Tag::CsrfInput.new(Marten::Template::Parser.new(""), raw_tag)
+        end
+      end
+
+      expect_raises(
+        Marten::Template::Errors::InvalidSyntax,
+        "Malformed csrf_input tag: 'as' keyword expected"
+      ) do
+        Marten::Template::Tag::CsrfInput.new(Marten::Template::Parser.new(""), "csrf_input to my_var")
+      end
+    end
+  end
+
+  describe "#render" do
+    it "returns the CSRF input if the handler is in the context" do
+      parser = Marten::Template::Parser.new("")
+
+      tag = Marten::Template::Tag::CsrfInput.new(parser, "csrf_input")
+      tag.should be_a Marten::Template::Tag::CsrfInput
+
+      handler = Marten::Template::Tag::CsrfInputSpec::TestHandler.new(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/test/xyz",
+            headers: HTTP::Headers{"Host" => "example.com"}
+          )
+        )
+      )
+      handler.process_dispatch
+
+      context = Marten::Template::Context.new
+      context.handler = handler
+
+      tag.render(context).matches?(/<input type="hidden" name="csrftoken" value=".+" \/>/).should be_true
+    end
+
+    it "returns an empty string if no handler is associated with the context" do
+      parser = Marten::Template::Parser.new("")
+
+      tag = Marten::Template::Tag::CsrfInput.new(parser, "csrf_input")
+      tag.should be_a Marten::Template::Tag::CsrfInput
+
+      handler = Marten::Template::Tag::CsrfInputSpec::TestHandler.new(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/test/xyz",
+            headers: HTTP::Headers{"Host" => "example.com"}
+          )
+        )
+      )
+      handler.process_dispatch
+
+      context = Marten::Template::Context.new
+
+      tag.render(context).should be_empty
+    end
+
+    it "assigns the CSRF token to the specified variable if applicable" do
+      parser = Marten::Template::Parser.new("")
+
+      tag = Marten::Template::Tag::CsrfInput.new(parser, "csrf_input as my_var")
+      tag.should be_a Marten::Template::Tag::CsrfInput
+
+      handler = Marten::Template::Tag::CsrfInputSpec::TestHandler.new(
+        Marten::HTTP::Request.new(
+          ::HTTP::Request.new(
+            method: "GET",
+            resource: "/test/xyz",
+            headers: HTTP::Headers{"Host" => "example.com"}
+          )
+        )
+      )
+      handler.process_dispatch
+
+      context = Marten::Template::Context.new
+      context.handler = handler
+
+      tag.render(context).should be_empty
+      context["my_var"].raw.should be_a Marten::Template::SafeString
+      context["my_var"].to_s.matches?(/<input type="hidden" name="csrftoken" value=".+" \/>/).should be_true
     end
   end
 end
--- src/marten/template/tag/csrf_input.cr
+++ src/marten/template/tag/csrf_input.cr
@@ -5,11 +5,20 @@ module Marten
     module Tag
       # The `csrf_input` template tag.
       #
-      # The `csrf_input` tag generates a hidden html input tag with `csrftoken` as name and
-      # the output of `csrf_token` tag as value.
+      # The `csrf_input` template tag allows generating a hidden HTML input tag containing the CSRF token.
+      #
+      # For example, using `{% csrf_input %}` in a template will generate the following HTML:
       #
       # ```html
-      # <input type="hidden" name="csrftoken" value="randomStrinGgenEratedbyCSRFtoken" />
+      # <input type="hidden" name="csrftoken" value="<csrfToken>" />
+      # ```
+      #
+      # Where `<csrfToken>` is the actual CSRF token.
+      #
+      # Optionally, the output of this tag can be assigned to a specific variable using the `as` keyword:
+      #
+      # ```
+      # {% csrf_input as my_csrf_input %}
       # ```
       class CsrfInput < Base
         include CanSplitSmartly
@@ -19,15 +28,28 @@ module Marten
         def initialize(_parser : Parser, source : String)
           parts = split_smartly(source)
 
-          if parts.size != 1
-            raise Errors::InvalidSyntax.new("Malformed csrf_input tag: takes no argument")
+          if parts.size == 2 || parts.size > 3
+            raise Errors::InvalidSyntax.new("Malformed csrf_input tag: either no arguments or two arguments expected")
+          end
+
+          if parts.size == 3 && parts[1] == "as"
+            @assigned_to = parts[2]
+          elsif parts.size == 3
+            raise Errors::InvalidSyntax.new("Malformed csrf_input tag: 'as' keyword expected")
           end
         end
 
         def render(context : Context) : String
           return "" if (handler = context.handler).nil?
 
-          %(<input type="hidden" name="csrftoken" value="#{handler.get_csrf_token}" />)
+          input = %(<input type="hidden" name="csrftoken" value="#{handler.get_csrf_token}" />)
+
+          if @assigned_to.nil?
+            input
+          else
+            context[@assigned_to.not_nil!] = SafeString.new(input)
+            ""
+          end
         end
       end
     end

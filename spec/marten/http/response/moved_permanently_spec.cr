6992cd8a3033dfddc6233ee3c15a13cc37427549
Morgan Aubert <morgan@aubert.email>
2024-05-31 17:38:18 -0400

    Fix failing spec

--- spec/marten/http/response/moved_permanently_spec.cr
+++ spec/marten/http/response/moved_permanently_spec.cr
@@ -5,7 +5,7 @@ describe Marten::HTTP::Response::MovedPermanently do
     it "allows to initialize a 301 HTTP response by specifying the target location" do
       response = Marten::HTTP::Response::MovedPermanently.new("https://example.com/foo/bar")
       response.status.should eq 301
-      response.headers.should eq({"Location" => "https://example.com/foo/bar"})
+      response.headers["Location"].should eq "https://example.com/foo/bar"
     end
   end
 end

15567325af56a199faae0b52455d759ceb23a776
Morgan Aubert <morgan@aubert.email>
2024-05-30 22:28:56 -0400

    Add missing docstrings for routing-related exceptions

--- src/marten/routing/errors.cr
+++ src/marten/routing/errors.cr
@@ -1,16 +1,22 @@
 module Marten
   module Routing
     module Errors
+      # Represents an error raised when a route is not found.
       class NoResolveMatch < Exception; end
 
+      # Represents an error raised when a route cannot be reversed.
       class NoReverseMatch < Exception; end
 
+      # Represents an error raised when an invalid route name is defined.
       class InvalidRuleName < Exception; end
 
+      # Represents an error raised when an invalid route path is defined.
       class InvalidRulePath < Exception; end
 
+      # Represents an error raised when an invalid route parameter is defined.
       class InvalidParameterName < Exception; end
 
+      # Represents an error raised when an unknown parameter type is being used in a route definition.
       class UnknownParameterType < Exception; end
     end
   end

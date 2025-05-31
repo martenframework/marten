4eaa30c7cb01bc45dd76818d5832320888454b72
Morgan Aubert <morgan@aubert.email>
2025-05-31 19:14:33 -0400

    Start 0.5.7 release notes

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -327,6 +327,7 @@ module.exports = {
             'the-marten-project/release-notes/0.5.4',
             'the-marten-project/release-notes/0.5.5',
             'the-marten-project/release-notes/0.5.6',
+            'the-marten-project/release-notes/0.5.7',
             'the-marten-project/release-notes/0.6',
           ],
         },

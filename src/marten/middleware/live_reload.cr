e7ea2eb5f59867024d7c842b063d76402a1f9114
John Jansen <john.jansen@me.com>
2025-09-08 13:06:34 +1200

    Add live reload middleware

--- src/marten/middleware/live_reload.cr
+++ src/marten/middleware/live_reload.cr
@@ -2,57 +2,45 @@ module Marten
   # Middleware that injects the live reload script into HTML responses.
   # This middleware is only active in development mode when live reload is enabled.
   class LiveReload < Middleware
-    @host : String
-    @port : Int32
-
-    def initialize
-      super
-      @host = Marten.settings.live_reload_host
-      @port = Marten.settings.live_reload_port
-    end
-
-    def call(request : Marten::HTTP::Request, get_response : Proc(Marten::HTTP::Response)) : Marten::HTTP::Response
+    def call(request : HTTP::Request, get_response : Proc(HTTP::Response)) : HTTP::Response
       response = get_response.call
 
       return response unless should_inject?(request, response)
 
       body = response.content.to_s
-      insert_at = body.rindex("</body>")
+      # Find the position of </body> tag (case-insensitive)
+      insert_at = body.downcase.rindex("</body>")
       return response unless insert_at
 
-      response.content = body.insert(insert_at, live_reload_script)
-      response
-    end
-
-    private def should_inject?(request : Marten::HTTP::Request, response : Marten::HTTP::Response)
-      return false unless Marten.settings.debug?
-      return false unless Marten.settings.live_reload_enabled?
-      return false unless response.content_type == "text/html"
-      return false if request.headers["X-Requested-With"]? == "XMLHttpRequest"
-      true
-    end
-
-    private def live_reload_script
-      <<-SCRIPT
+      script = <<-SCRIPT
         <script>
           (function() {
-            let ws = new WebSocket('ws://#{@host}:#{@port}/live_reload');
-            
-            ws.onmessage = function(msg) {
-              if (msg.data === 'reload') {
-                window.location.reload();
-              }
+            const events = new EventSource('/live-reload');
+            events.onmessage = function(e) {
+              window.location.reload();
             };
-            
-            ws.onclose = function() {
-              // Try to reconnect if server temporarily down
+            events.onerror = function() {
+              events.close();
               setTimeout(() => {
-                ws = new WebSocket('ws://#{@host}:#{@port}/live_reload');
+                const newEvents = new EventSource('/live-reload');
+                events.onmessage = events.onmessage;
+                events.onerror = events.onerror;
               }, 1000);
             };
           })();
         </script>
       SCRIPT
+
+      response.content = body.insert(insert_at, script)
+      response
+    end
+
+    private def should_inject?(request : HTTP::Request, response : HTTP::Response)
+      return false unless Marten.settings.debug?
+      return false unless Marten.settings.live_reload_enabled?
+      return false unless response.content_type.downcase.starts_with?("text/html")
+      return false if request.headers["X-Requested-With"]? == "XMLHttpRequest"
+      true
     end
   end
 end

8f8cdc5b76ee23265f1710076bd87cbfdaa71595
Morgan Aubert <morgan@aubert.email>
2026-08-04 19:33:42 -0400

    Improve thread-safety of the cached template loader

--- src/marten/template/loader/cached.cr
+++ src/marten/template/loader/cached.cr
@@ -5,11 +5,17 @@ module Marten
       class Cached < Base
         def initialize(@loaders : Array(Loader::Base))
           @templates_cache = Hash(String, Template).new
+          @mutex = Mutex.new
         end
 
         def get_template(template_name) : Template
-          template = @templates_cache[template_name]?
-          return template unless template.nil?
+          @mutex.synchronize do
+            if template = @templates_cache[template_name]?
+              return template
+            end
+          end
+
+          template = nil
 
           @loaders.each do |loader|
             begin
@@ -21,12 +27,14 @@ module Marten
             break unless template.nil?
           end
 
-          unless template.nil?
-            @templates_cache[template_name] = template
-            return template
+          if template.nil?
+            raise Errors::TemplateNotFound.new("Template #{template_name} could not be found")
           end
 
-          raise Errors::TemplateNotFound.new("Template #{template_name} could not be found")
+          # Store under the lock; another fiber may have cached the same template meanwhile.
+          @mutex.synchronize do
+            @templates_cache[template_name] ||= template
+          end
         end
 
         def get_template_source(template_name) : String
